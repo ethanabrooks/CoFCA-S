4cb5cd5e1f1cde477ed53e4563d9692237525638
Ethan Brooks <ethanabrooks@gmail.com>
2018-12-31 16:27:22 -0500

    delete bench.monitor stuff

--- ppo/envs.py
+++ ppo/envs.py
@@ -69,12 +69,6 @@ def make_env(env_id, seed, rank, log_dir, add_timestep, allow_early_resets,
                 obs_shape) == 1 and str(env).find('TimeLimit') > -1:
             env = AddTimestep(env)
 
-        if log_dir is not None:
-            env = bench.Monitor(
-                env,
-                os.path.join(log_dir, str(rank)),
-                allow_early_resets=allow_early_resets)
-
         if is_atari:
             if len(env.observation_space.shape) == 3:
                 raise NotImplementedError
