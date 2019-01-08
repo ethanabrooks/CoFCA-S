add1e87c0f6894d633cbe5ef46b9c1717c582d2f
Ethan Brooks <ethanabrooks@gmail.com>
2019-01-08 15:34:47 -0500

    add scripts/dist

---
+++ scripts/dist.py
@@ -0,0 +1,30 @@
+import torch
+from tensorboardX import SummaryWriter
+import itertools
+import sys
+
+N_INPUTS = 5
+network = torch.nn.Linear(N_INPUTS, 2)
+softplus = torch.nn.Softplus()
+inputs = torch.ones(1, N_INPUTS)
+optimizer = torch.optim.SGD(network.parameters(), lr=.01)
+log_dir = sys.argv[1]
+writer = SummaryWriter(log_dir=f'/tmp/dist/{log_dir}')
+
+for i in itertools.count():
+    a, b = torch.chunk(network(inputs), 2, dim=-1)
+    dist = torch.distributions.Normal(a, softplus(b))
+    sample = dist.sample()
+    j = torch.norm(sample, dim=-1)
+    log_prob = dist.log_prob(sample)
+    loss = log_prob * j
+    writer.add_scalar('loss', loss, i)
+    writer.add_scalar('log_prob', log_prob, i)
+    writer.add_scalar('J', j, i)
+    if i % 100 == 0:
+        print(f'loss: {loss}\tJ: {j}')
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+
