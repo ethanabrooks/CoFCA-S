98d34b0981dfebcf8bcb981009a0a5068c306f5b
Ethan Brooks <ethanabrooks@gmail.com>
2019-01-09 15:57:31 -0500

    eliminate repeat_sampling in gan

--- ppo/gan.py
+++ ppo/gan.py
@@ -1,26 +1,29 @@
-from gym.spaces import Box
 import torch
 import torch.nn as nn
+from gym.spaces import Box
 
 from ppo.utils import mlp
 from utils import space_to_size
 
+from ppo.utils import mlp
+
 
 class GAN(nn.Module):
     def __init__(self, goal_space: Box, hidden_size, learning_rate: float,
-                 **kwargs):
+                 entropy_coef: float, **kwargs):
         super().__init__()
         self.learning_rate = learning_rate
+        self.entropy_coef = entropy_coef
         self.goal_space = goal_space
         goal_size = space_to_size(goal_space)
         self.hidden_size = hidden_size
-        self.repeat_sample = 5
         self.network = nn.Sequential(
             mlp(num_inputs=hidden_size,
                 hidden_size=hidden_size,
                 num_outputs=2 * goal_size,
                 name='gan',
-                **kwargs), torch.nn.Softplus())
+                **kwargs))
+        self.softplus = torch.nn.Softplus()
         self.regularizer = None
 
     def goal_input(self, num_outputs):
@@ -30,11 +33,9 @@ class GAN(nn.Module):
         raise NotImplementedError
 
     def dist(self, num_inputs):
-        network_out = self.network(self.goal_input(num_inputs))
-        network_out = network_out.view(1, num_inputs, -1).expand(
-            self.repeat_sample, -1, -1)
-        params = torch.chunk(network_out, 2, dim=-1)
-        return torch.distributions.Beta(*params)
+        network_out = self.softplus(self.network(self.goal_input(num_inputs)))
+        a, b = torch.chunk(network_out, 2, dim=-1)
+        return torch.distributions.Beta(a, b)
 
     def log_prob(self, goal):
         num_inputs = goal.size()[0]
@@ -42,21 +43,20 @@ class GAN(nn.Module):
 
     def sample(self, num_outputs):
         dist = self.dist(num_outputs)
-        samples = dist.sample().mean(dim=0)
+        samples = dist.sample()
         high = torch.tensor(self.goal_space.high)
         low = torch.tensor(self.goal_space.low)
         goals = samples * (high - low) + low
-        mean_log_prob = dist.log_prob(dist.mean).mean()
+        log_prob = dist.log_prob(samples).sum(dim=-1).exp()
         if self.regularizer is None:
-            self.regularizer = mean_log_prob
+            self.regularizer = log_prob.mean()
         else:
-            self.regularizer += .01 * (mean_log_prob - self.regularizer)
-        importance_weighting = (
-            self.regularizer - dist.log_prob(samples)[0]).sum(dim=-1).exp()
+            self.regularizer += .01 * (log_prob.mean() - self.regularizer)
+        importance_weighting = self.regularizer / log_prob
         return samples, goals, importance_weighting.view(-1, 1)
 
-    def parameters(self):
-        return self.network.parameters()
+    def parameters(self, **kwargs):
+        return self.network.parameters(**kwargs)
 
     def to(self, device):
         self.network.to(device)
