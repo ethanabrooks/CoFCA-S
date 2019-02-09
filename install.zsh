73b9b3b276f3e02be01ecfbbf48a454235ff2e0a
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-08 21:23:27 -0500

    fix git stuff in install.zsh

--- install.zsh
+++ install.zsh
@@ -2,12 +2,14 @@
 
 pip install --upgrade pip numpy cython ipython ipdb yapf isort pylint
 repos=(mujoco rl-utils hsr-env gridworld-env lab-notebook)
+here=$pwd
 for repo in $repos; do
   dir="../$repo"
   if [ ! -d $dir ]; then
     git clone "git@github.com:lobachevzky/$repo" "../$repo"
   fi 
-  git -C "$dir/.git" pull
-  pip install -e "$dir"
+  cd $dir && git pull
+  pip install -e .
 done
-pip install -e .
+cd $here
+pip install -e $here
