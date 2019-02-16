b125e70580d77800b1cef66a3117fb43ae23ecd9
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-16 16:49:45 +0000

    take out print in update()

--- ppo/update.py
+++ ppo/update.py
@@ -155,13 +155,12 @@ class PPO:
                 logits = torch.ones_like(grads)
             else:
                 logits = grads
-            print(logits)
 
             dist = Categorical(logits=logits)
             goal_to_train = dist.sample().float()
             goals_trained.append(goal_to_train)
-            importance_weighting = 1 / (unique.numel() * dist.log_prob(
-                goal_to_train).exp())
+            importance_weighting = 1 / (
+                unique.numel() * dist.log_prob(goal_to_train).exp())
 
             uses_goal = batches.goals.squeeze() == goal_to_train
             indices = torch.arange(total_batch_size)[uses_goal]
