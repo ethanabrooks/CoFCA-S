849563393b0221587387dc704283ee4c00d086d0
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-20 17:43:45 -0500

    update install.zsh

--- install.zsh
+++ install.zsh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 
 pip install --upgrade pip numpy cython ipython ipdb yapf isort pylint
-repos=(mujoco rl-utils hsr-env gridworld-env lab-notebook)
-here=$(pwd)
+repos=(mujoco rl-utils hsr-env gridworld-env lab-notebook ppo)
 for repo in $repos; do
   dir="../$repo"
   if [ ! -d $dir ]; then
@@ -11,4 +10,3 @@ for repo in $repos; do
   cd $dir && git pull
   pip install -e .
 done
-pip install -e $here
