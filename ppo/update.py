dd5e6c84f8af3fc4f872a5910ad4c56c639ece59
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-21 13:49:16 +0000

    Revert "add grad_fraction"
    This reverts commit 9192d37437565d789e3706eb38a6ebd4f7c1a738.

--- ppo/update.py
+++ ppo/update.py
@@ -43,7 +43,6 @@ class PPO:
                  value_loss_coef,
                  entropy_coef,
                  temperature,
-                 grad_fraction,
                  sampling_strategy,
                  use_value,
                  learning_rate=None,
@@ -52,7 +51,6 @@ class PPO:
                  use_clipped_value_loss=True,
                  task_generator=None):
 
-        self.grad_fraction = grad_fraction
         self.use_value = use_value
         self.sampling_strategy = sampling_strategy
         self.temperature = temperature
@@ -167,8 +165,7 @@ class PPO:
             elif self.sampling_strategy == '0/1logits':
                 logits = torch.ones_like(grads) * -self.temperature
                 sorted_grads, _ = torch.sort(grads)
-                mid_grad = sorted_grads[int(
-                    grads.numel() * self.grad_fraction)]
+                mid_grad = sorted_grads[grads.numel() // 4]
                 logits[grads > mid_grad] = self.temperature
             elif self.sampling_strategy == 'experiment':
                 logits = grads * self.temperature
