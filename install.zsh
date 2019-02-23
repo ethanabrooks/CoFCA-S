7b582474447aeee08d3b6b6a3c52e52ec734611c
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-23 04:17:10 +0000

    install.zsh

--- install.zsh
+++ install.zsh
@@ -1,11 +1,6 @@
 #!/usr/bin/env zsh
 set -e
 
-conda upgrade conda
-conda install numpy cython ipython yapf isort pylint tensorflow pytorch
-pip install --upgrade pip
-pip install ipdb
-
 repos=(mujoco rl-utils hsr-env gridworld-env lab-notebook ppo)
 for repo in $repos; do
   dir="../$repo"
