95736cb2ebeb4be7cd985d7a363fa32bf43446ab
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-28 14:24:24 -0500

    put temperature back into grads

--- ppo/task_generator.py
+++ ppo/task_generator.py
@@ -10,9 +10,10 @@ class TaskGenerator(NoInput):
         self.entropy_coef = entropy_coef
         self.task_size = task_size
         self.softmax = torch.nn.Softmax(dim=-1)
+        self.temperature = 10
 
     def probs(self):
-        return self.softmax(self.weight).view(self.task_size)
+        return self.softmax(self.temperature * self.weight).view(self.task_size)
 
     def importance_weight(self, task_index):
         return 1 / (self.task_size * self.probs()[task_index]).detach()
