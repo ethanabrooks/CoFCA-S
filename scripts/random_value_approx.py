4560d285d3efe3da04397cc83226006ea008e524
Ethan Brooks <ethanabrooks@gmail.com>
2019-05-04 17:47:25 -0400

    got things working with config file

--- scripts/random_value_approx.py
+++ scripts/random_value_approx.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+import configparser
+import json
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -71,14 +74,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--distribution', type=get_distribution)
     parser.add_argument('--stats', nargs='*', type=float)
-    parser.add_argument('--num-values', type=int, required=True)
-    parser.add_argument('--num-steps', type=int, default=100)
-    parser.add_argument('--seed', type=int)
-    parser.add_argument('--exploration-bonus', type=float, required=True)
-    parser.add_argument('--num-seeds', type=int, default=10)
-    parser.add_argument('--noise-scale', type=float, default=1)
+    parser.add_argument('--num-values', type=int, default=20)
+    parser.add_argument('--num-steps', type=int, default=50)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--exploration-bonus', type=float, default=.01)
+    parser.add_argument('--num-seeds', type=int, default=100)
+    parser.add_argument('--noise-scale', type=float, default=.01)
     parser.add_argument('--noise-std', type=float, default=1)
     parser.add_argument('--noise-mean', type=float, default=1)
-    parser.add_argument('--num-samples', type=float, default=20)
-    # parser.add_argument('--values', nargs='*', type=float)
+    parser.add_argument('--num-samples', type=float, default=1)
+
+
+    if len(sys.argv) == 2:
+        import ipdb; ipdb.set_trace()
+        with open(sys.argv.pop(1)) as f:
+            parser.set_defaults(**json.load(f))
+
+
     main(**vars(parser.parse_args()))
