43594993f6e6b5a363154af53b08b379f5695b4c
Ethan Brooks <ethanabrooks@gmail.com>
2019-05-28 20:22:59 -0400

    2 x2-count subtasks

--- gridworld_env/tasks_gridworld.py
+++ gridworld_env/tasks_gridworld.py
@@ -51,8 +51,8 @@ class TasksGridWorld(gym.Env):
             'visit',
             'pick-up',
             'transform',
-            # 'pick-up-2',
-            # 'transform-2',
+            'pick-up-2',
+            'transform-2',
         ]
         # 'pick-up-3',
         # 'transform-3', ]
@@ -61,8 +61,8 @@ class TasksGridWorld(gym.Env):
             1,
             1,
             1,
-            # 2,
-            # 2,
+            2,
+            2,
             # 3, 3,
         ]
 
