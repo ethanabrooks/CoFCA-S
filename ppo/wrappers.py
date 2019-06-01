6c86a7499678fa90dfa19480a48539b9c8617ed3
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-01 16:23:11 -0400

    add OneHotWrapper

--- ppo/wrappers.py
+++ ppo/wrappers.py
@@ -183,6 +183,25 @@ class VecPyTorchFrameStack(VecEnvWrapper):
         self.venv.to(device)
 
 
+class OneHotWrapper(gym.Wrapper):
+    def wrap_observation(self, obs, observation_space=None):
+        if observation_space is None:
+            observation_space = self.observation_space
+        if isinstance(observation_space, spaces.Discrete):
+            return onehot(obs, observation_space.n)
+        if isinstance(observation_space, spaces.MultiDiscrete):
+            assert observation_space.contains(obs)
+
+            def one_hots():
+                nvec = observation_space.nvec
+                for o, n in zip(
+                        obs.reshape(len(obs), -1).T,
+                        nvec.reshape(len(nvec), -1).T):
+                    yield onehot(o, n)
+
+            return np.concatenate(list(one_hots()), axis=-1)
+
+
 def get_vec_normalize(venv):
     if isinstance(venv, VecNormalize):
         return venv
