76ea458af48da444a43622a9681824333fc46577
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-01 09:52:24 -0400

    just assign all runs to cuda:0 to test non-determinism

--- ppo/train.py
+++ ppo/train.py
@@ -88,7 +88,8 @@ class Trainer:
         device = 'cpu'
         if cuda:
             tick = time.time()
-            device = torch.device('cuda', get_random_gpu())
+            # device = torch.device('cuda', get_random_gpu())
+            device = torch.device('cuda', 0)
             envs.to(device)
             actor_critic.to(device)
             rollouts.to(device)
