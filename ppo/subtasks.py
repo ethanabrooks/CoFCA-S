1499222210b632c6eaf3ca648e0354a422eed44b
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-07 12:33:17 -0400

    fix aux_loss

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -227,10 +227,11 @@ class SubtasksAgent(Agent, NNBase):
             imitation_dist = self.imitation_agent(inputs, rnn_hxs, masks).dist
             imitation_probs = imitation_dist.probs.detach().unsqueeze(1)
             log_probs = torch.log(dist.probs).unsqueeze(2)
-            imitation_loss = -(imitation_probs @ log_probs).view(-1)
-            imitation_loss += imitation_loss.detach() * hx.log_prob.view(-1)   # TODO: is this right?
-            aux_loss += imitation_loss + imitation_loss
-            losses.update(imitation_loss=imitation_loss)
+            imitation_obj = (imitation_probs @ log_probs).view(-1)
+            aux_loss -= (
+                imitation_obj + imitation_obj.detach() * hx.log_prob.flatten()
+            )  # TODO: is this right?
+            losses.update(imitation_obj=imitation_obj)
 
         return AgentValues(
             value=value,
