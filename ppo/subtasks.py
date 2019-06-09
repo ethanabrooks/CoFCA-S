c918cc370b1fa6e032a25851759dd11a8e172fc2
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-09 08:36:13 -0400

    feed pi_theta r_target

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -428,7 +428,7 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
 
             # TODO: deterministic
             # g
-            dist = self.pi_theta((h, r))
+            dist = self.pi_theta((h, r_target))
             g_int = dist.sample()
             outputs.g_int.append(g_int.float())
             outputs.g_probs.append(dist.probs)
