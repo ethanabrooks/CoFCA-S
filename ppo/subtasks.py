2da2274429e07cad0ebee3f6cbeb61b9b7ee6e4f
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-11 14:33:37 -0400

    Revert "do not hard code c"
    This reverts commit 657f24493e36ecafb672bc00f9e2cc7f5034719e.

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -393,6 +393,7 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
                     reduction='none',
                 ))
 
+            c = next_subtask[i]  #TODO
             outputs.c.append(c)
 
             # TODO: figure this out
