c41e7869bab8c65738033be3448feed696338994
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-11 22:09:44 -0400

    sample c, despite hard-coding it subsequently

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -404,13 +404,13 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
 
             s = self.f(torch.cat([obs[i], r, g, b], dim=-1))
             logits = self.phi_update(torch.cat([s, h], dim=-1))
-            # if self.hard_update:
-            # dist = FixedCategorical(logits=logits)
-            # c = dist.sample().float()
-            # outputs.c_probs.append(dist.probs)
-            # else:
-            c = torch.sigmoid(logits[:, :1])
-            outputs.c_probs.append(torch.zeros_like(logits))  # dummy value
+            if self.hard_update:
+                dist = FixedCategorical(logits=logits)
+                c = dist.sample().float()
+                outputs.c_probs.append(dist.probs)
+            else:
+                c = torch.sigmoid(logits[:, :1])
+                outputs.c_probs.append(torch.zeros_like(logits))  # dummy value
 
             # c_loss
             outputs.c_loss.append(
