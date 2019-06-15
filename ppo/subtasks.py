4b629ffe83d3d5d5801e7302bcffeb67cdf35905
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-15 13:12:21 -0400

    Revert "new-sampling"
    This reverts commit 2be41b4625da49e3e1ceb496e42163af8c7635f5.

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -547,10 +547,7 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
 
             # TODO: deterministic
             # g
-            probs = self.pi_theta((h, r)).probs
-            g_one_hot = self.g_one_hots[hx.g_int.long().flatten()]
-            dist = FixedCategorical(probs=interp(g_one_hot, probs, c))
-            # dist = self.pi_theta((h, r))
+            dist = self.pi_theta((h, r))
             g = dist.sample()
             outputs.g_int.append(g.float())
             outputs.g_probs.append(dist.probs)
@@ -565,8 +562,7 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
                 reduction='none',
             )
             outputs.g_loss.append(torch.mean(g_loss, dim=-1, keepdim=True))
-            # g_embed1 = interp(g_embed1, g_embed2, c)
-            g_embed1 = g_embed2
+            g_embed1 = interp(g_embed1, g_embed2, c)
             outputs.g_embed.append(g_embed1)
 
             # b
