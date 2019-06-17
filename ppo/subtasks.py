228bc81949b5314b82531e5418fd542ab45372a5
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-17 17:58:08 -0400

    move c_loss computation under else clause for hard_update

--- ppo/subtasks.py
+++ ppo/subtasks.py
@@ -478,21 +478,23 @@ class SubtasksRecurrence(torch.jit.ScriptModule):
                 outputs.c_loss.append(-dist.log_probs(next_subtask[i]))
             else:
                 c = torch.sigmoid(logits)
+                if torch.any(next_subtask[i] > 0):
+                    weight = torch.ones_like(c)
+                    weight[next_subtask[i] > 0] /= torch.sum(
+                        next_subtask[i] > 0)
+                    weight[next_subtask[i] == 0] /= torch.sum(
+                        next_subtask[i] == 0)
+
+                    outputs.c_loss.append(
+                        F.binary_cross_entropy(
+                            torch.clamp(c, 0., 1.),
+                            next_subtask[i],
+                            weight=weight,
+                            reduction='none'))
+                else:
+                    outputs.c_loss.append(torch.zeros_like(c))
+
             outputs.c_truth.append(next_subtask[i])
-
-            if torch.any(next_subtask[i] > 0):
-                weight = torch.ones_like(c)
-                weight[next_subtask[i] > 0] /= torch.sum(next_subtask[i] > 0)
-                weight[next_subtask[i] == 0] /= torch.sum(next_subtask[i] == 0)
-
-                outputs.c_loss.append(
-                    F.binary_cross_entropy(
-                        torch.clamp(c, 0., 1.),
-                        next_subtask[i],
-                        weight=weight,
-                        reduction='none'))
-            else:
-                outputs.c_loss.append(torch.zeros_like(c))
 
             outputs.c.append(c)
 
