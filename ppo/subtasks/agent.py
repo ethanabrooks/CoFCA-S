7064d36195b6d2a0463d2c8d6b2abf1b8b252504
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-27 00:38:13 -0400

    learn phi_theta instead of c

--- ppo/subtasks/agent.py
+++ ppo/subtasks/agent.py
@@ -373,6 +373,7 @@ class Recurrence(torch.jit.ScriptModule):
             # cg
             g_binary = M[torch.arange(N), G[t]]
             cg, cg_loss, cg_probs = phi_update(subtask_param=g_binary, )
+            cg = cr = next_subtask[t]
 
             # p
             p2 = update_attention(p, t)
