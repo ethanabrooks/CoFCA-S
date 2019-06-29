29b43282420ac91cd1b9704ad3fa57071aa4f8e1
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-29 13:10:33 -0400

    fix __init__

--- gridworld_env/subtasks_gridworld.py
+++ gridworld_env/subtasks_gridworld.py
@@ -14,6 +14,7 @@ import six
 from ppo.utils import set_index
 
 Subtask = namedtuple("Subtask", "interaction count object")
+Obs = namedtuple("Obs", "base subtask subtasks next_subtask")
 
 
 class SubtasksGridWorld(gym.Env):
