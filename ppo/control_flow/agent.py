e9aff28bc40c9b63d1c94f9325b5edc6f697a14e
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-30 18:32:10 -0400

    learn minimal phi_shift

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -134,8 +134,7 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
                 N, -1
             )  # TODO
             truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
-            pred = self.phi_shift(phi_in)
-            pred = truth
+            pred = self.phi_shift(phi_in).unsqueeze(-1)
             trans = pred * true_path + (1 - pred) * false_path
             return (p.unsqueeze(1) @ trans).squeeze(1)
 
