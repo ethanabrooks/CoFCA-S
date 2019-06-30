fix truth computation in phi_update()

ppo/subtasks/agent.py
@@ -400,15 +400,16 @@ class Recurrence(torch.jit.ScriptModule):
                     subtask_param, tuple(self.subtask_nvec), dim=-1
                 )
                 parts = (debug_obs, self.a_one_hots[A[t - 1]]) + task_sections
-                # a_one_hot = self.a_one_hots[A[t - 1]]
-                # correct_object = obj * debug_obs[:, : self.subtask_nvec[2]]
-                # column1 = interaction[:, :1]
-                # column2 = interaction[:, 1:] * a_one_hot[:, 4:]
-                # correct_action = torch.cat([column1, column2], dim=-1)
-                # truth = (
-                # correct_action.sum(-1, keepdim=True)
-                # * correct_object.sum(-1, keepdim=True)
-                # ).detach()
+                a_one_hot = self.a_one_hots[A[t - 1]]
+                interaction, count, obj = task_sections
+                correct_object = obj * debug_obs[:, 1 : 1 + self.subtask_nvec[2]]
+                column1 = interaction[:, :1]
+                column2 = interaction[:, 1:] * a_one_hot[:, 4:]
+                correct_action = torch.cat([column1, column2], dim=-1)
+                truth = (
+                    correct_action.sum(-1, keepdim=True)
+                    * correct_object.sum(-1, keepdim=True)
+                ).detach()
                 if self.multiplicative_interaction:
                     c_logits = self.phi_update(parts)
                 else:
@@ -432,7 +433,8 @@ class Recurrence(torch.jit.ScriptModule):
                     loss = F.binary_cross_entropy(
                         torch.clamp(c, 0.0, 1.0), next_subtask[t], reduction="none"
                     )
-                return c, loss, probs
+                return truth, loss, probs  # TODO
+                # return c, loss, probs
 
             # cr
             cr, cr_loss, cr_probs = phi_update(subtask_param=r)

ppo/subtasks/wrappers.py
@@ -23,10 +23,10 @@ class DebugWrapper(gym.Wrapper):
         truth = int(self.env.unwrapped.subtask_idx)
         guess = int(actions.g)
         r = float(np.all(guess == truth)) - 1
-        # if r < 0:
-        # import ipdb
+        if r < 0:
+            import ipdb
 
-        # ipdb.set_trace()
+            ipdb.set_trace()
         s, _, t, i = super().step(action)
         self.last_guess = guess
         self.last_reward = r