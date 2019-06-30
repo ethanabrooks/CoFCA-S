3a4d1debc344a0b0396264a3bf06d720742ee643
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-30 12:45:02 -0400

    bring in commented out truth computation in subtasks/agent/phi_shift

--- ppo/subtasks/agent.py
+++ ppo/subtasks/agent.py
@@ -400,6 +400,15 @@ class Recurrence(torch.jit.ScriptModule):
                     subtask_param, tuple(self.subtask_nvec), dim=-1
                 )
                 parts = (debug_obs, self.a_one_hots[A[t - 1]]) + task_sections
+                # a_one_hot = self.a_one_hots[A[t - 1]]
+                # correct_object = obj * debug_obs[:, : self.subtask_nvec[2]]
+                # column1 = interaction[:, :1]
+                # column2 = interaction[:, 1:] * a_one_hot[:, 4:]
+                # correct_action = torch.cat([column1, column2], dim=-1)
+                # truth = (
+                # correct_action.sum(-1, keepdim=True)
+                # * correct_object.sum(-1, keepdim=True)
+                # ).detach()
                 if self.multiplicative_interaction:
                     c_logits = self.phi_update(parts)
                 else:
