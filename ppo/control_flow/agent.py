c9796bae1169dc3178daff68f06ced9baa453948
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-30 20:30:27 -0400

    add additional layer after max pool

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -39,11 +39,11 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
             # init_(nn.Linear(num_object_types * d * h * w, 1), "sigmoid"),
             # Reshape(-1, in_channels, *self.obs_shape[-2:]),
             init_(
-                nn.Conv2d(num_object_types * d, 1, kernel_size=1, stride=1), "sigmoid"
+                nn.Conv2d(num_object_types * d, hidden_size, kernel_size=1, stride=1)
             ),
             nn.MaxPool2d(kernel_size=self.obs_shape[-2:], stride=1),
             Flatten(),
-            # init_(nn.Linear(hidden_size, 1), "sigmoid"),
+            init_(nn.Linear(hidden_size, 1), "sigmoid"),
             nn.Sigmoid(),
             Reshape(-1, 1, 1),
         )
