98df6b9261cb6fdd2816133d1a3cd187ee78b2d8
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-01 16:31:47 -0400

    delete repeated TODO code in subtasks Agent

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -26,25 +26,25 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
         self.n_conditions = self.obs_spaces.conditions.shape[0]
 
         d, h, w = self.obs_shape
-        # self.phi_shift = nn.Sequential(
-        # # Reshape(-1, num_object_types * d, h, w),
-        # Parallel(
-        # nn.Sequential(Reshape(-1, 1, d, h, w)),
-        # nn.Sequential(Reshape(-1, num_object_types, 1, 1, 1)),
-        # ),
-        # Product(),
-        # Reshape(-1, num_object_types * d * h * w),
-        # init_(nn.Linear(num_object_types * d * h * w, 1), "sigmoid"),
-        # # Reshape(-1, in_channels, *self.obs_shape[-2:]),
-        # # init_(
-        # # nn.Conv2d(num_object_types * d, hidden_size, kernel_size=1, stride=1)
-        # # ),
-        # # nn.MaxPool2d(kernel_size=self.obs_shape[-2:], stride=1),
-        # # Flatten(),
-        # # init_(nn.Linear(hidden_size, 1), "sigmoid"),
-        # nn.Sigmoid(),
-        # Reshape(-1, 1, 1),
-        # )
+        self.phi_shift = nn.Sequential(
+            # Reshape(-1, num_object_types * d, h, w),
+            Parallel(
+                nn.Sequential(Reshape(-1, 1, d, h, w)),
+                nn.Sequential(Reshape(-1, num_object_types, 1, 1, 1)),
+            ),
+            Product(),
+            Reshape(-1, num_object_types * d * h * w),
+            init_(nn.Linear(num_object_types * d * h * w, 1), "sigmoid"),
+            # Reshape(-1, in_channels, *self.obs_shape[-2:]),
+            # init_(
+            # nn.Conv2d(num_object_types * d, hidden_size, kernel_size=1, stride=1)
+            # ),
+            # nn.MaxPool2d(kernel_size=self.obs_shape[-2:], stride=1),
+            # Flatten(),
+            # init_(nn.Linear(hidden_size, 1), "sigmoid"),
+            nn.Sigmoid(),
+            Reshape(-1, 1, 1),
+        )
         self.obs_shapes = Obs(
             base=self.obs_spaces.base.shape,
             subtask=[1],
