799312d6fd7e2a1b398efd9c767b51d8868674f9
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-30 20:23:38 -0400

    pass in all of inputs.base[t]

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -31,12 +31,12 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
         self.phi_shift = nn.Sequential(
             # Reshape(-1, num_object_types * d, h, w),
             Parallel(
-                nn.Sequential(Reshape(-1, 1, num_object_types, h, w)),
+                nn.Sequential(Reshape(-1, 1, d, h, w)),
                 nn.Sequential(Reshape(-1, num_object_types, 1, 1, 1)),
             ),
             Product(),
-            Reshape(-1, num_object_types ** 2 * h * w),
-            init_(nn.Linear(num_object_types ** 2 * h * w, 1), "sigmoid"),
+            Reshape(-1, num_object_types * d * h * w),
+            init_(nn.Linear(num_object_types * d * h * w, 1), "sigmoid"),
             # Reshape(-1, in_channels, *self.obs_shape[-2:]),
             # init_(
             # nn.Conv2d(num_object_types * d, hidden_size, kernel_size=1, stride=1)
@@ -137,7 +137,7 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
                 inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
             ).view(N, -1)
             # truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
-            pred = self.phi_shift((inputs.base[t, :, 1:-2], c))
+            pred = self.phi_shift((inputs.base[t], c))
             trans = pred * true_path + (1 - pred) * false_path
             return (p.unsqueeze(1) @ trans).squeeze(1)
 
