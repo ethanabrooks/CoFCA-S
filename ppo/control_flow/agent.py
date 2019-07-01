4635fe606510a371e275bb8dad45cca02eb6ab38
Ethan Brooks <ethanabrooks@gmail.com>
2019-06-30 23:44:50 -0400

    hard-code pred

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -136,8 +136,9 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
             phi_in = (
                 inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
             ).view(N, -1)
-            # truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
-            pred = self.phi_shift((inputs.base[t], c))
+            truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
+            # pred = self.phi_shift((inputs.base[t], c))
+            pred = truth
             trans = pred * true_path + (1 - pred) * false_path
             return (p.unsqueeze(1) @ trans).squeeze(1)
 
