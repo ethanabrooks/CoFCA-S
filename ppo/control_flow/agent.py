9dace53cfb2ec483f842ebbb77779a4e5d119342
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-02 09:14:18 -0400

    comment out truth code

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -73,18 +73,17 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
         false_path = self.branch_one_hots[false_path.squeeze(-1).long()]
 
         def update_attention(p, t):
-            x = inputs
             c = (p.unsqueeze(1) @ conditions).squeeze(1)
             # c = conditions[
             # torch.arange(N, device=c.device), inputs.subtask[t].long().flatten()
             # ]
-            phi_in = (
-                inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
-            ).view(N, -1)
-            truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
-            phi_in = c.view(N, conditions.size(2), 1, 1, 1) * inputs.base[t].unsqueeze(
-                1
-            )
+            # phi_in = (
+            # inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
+            # ).view(N, -1)
+            # truth = torch.any(phi_in > 0, dim=-1).float().view(N, 1, 1)
+            # phi_in = c.view(N, conditions.size(2), 1, 1, 1) * inputs.base[t].unsqueeze(
+            #     1
+            # )
             pred = self.phi_shift((inputs.base[t], c))  # TODO
             trans = pred * true_path + (1 - pred) * false_path
             return (p.unsqueeze(1) @ trans).squeeze(1)
