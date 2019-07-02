d8ff389b4320ea58d5d5870ecb17c1f24ad00bc1
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-01 22:33:47 -0400

    revert to best version (relu after pooling)

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -37,14 +37,12 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
             # init_(nn.Linear(num_object_types * d * h * w, 1), "sigmoid"),
             Reshape(-1, d * num_object_types, *self.obs_shape[-2:]),
             init_(
-                nn.Conv2d(
-                    num_object_types * d, hidden_size // 2, kernel_size=1, stride=1
-                )
+                nn.Conv2d(num_object_types * d, hidden_size, kernel_size=1, stride=1)
             ),
             nn.MaxPool2d(kernel_size=self.obs_shape[-2:], stride=1),
             nn.ReLU(),
             Flatten(),
-            init_(nn.Linear(hidden_size // 2, 1), "sigmoid"),
+            init_(nn.Linear(hidden_size, 1), "sigmoid"),
             nn.Sigmoid(),
             Reshape(-1, 1, 1),
         )
