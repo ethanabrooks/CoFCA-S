611533230e5c5f09061f97e9af4869228f1db4f4
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-02 23:35:34 -0400

    hard-code success reward

--- ppo/train.py
+++ ppo/train.py
@@ -21,13 +21,7 @@ from ppo.storage import RolloutStorage
 from ppo.subtasks.wrappers import Wrapper
 from ppo.update import PPO
 from ppo.utils import get_n_gpu, get_random_gpu
-from ppo.wrappers import (
-    AddTimestep,
-    TransposeImage,
-    VecNormalize,
-    VecPyTorch,
-    VecPyTorchFrameStack,
-)
+from ppo.wrappers import AddTimestep, TransposeImage, VecNormalize, VecPyTorch, VecPyTorchFrameStack
 
 try:
     import dm_control2gym
@@ -273,15 +267,12 @@ class Train:
             counter["time_step"] += np.ones_like(done)
             episode_rewards = counter["reward"][done]
             episode_counter["rewards"] += list(episode_rewards)
-            if np.any(reward.numpy()[done] < 0):
-                import ipdb
+            episode_counter["success"] += list(reward.numpy()[done] > 0)
 
-                ipdb.set_trace()
-
-            if self.success_reward is not None:
-                episode_counter["success"] += list(
-                    episode_rewards >= self.success_reward
-                )
+            # if self.success_reward is not None:
+            # episode_counter["success"] += list(
+            # episode_rewards >= self.success_reward
+            # )
             episode_counter["time_steps"] += list(counter["time_step"][done])
             counter["reward"][done] = 0
             counter["time_step"][done] = 0
