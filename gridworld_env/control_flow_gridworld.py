8692f7c978e35ca44488d67a3dbd2a52ed34426a
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-04 18:39:40 -0400

    remove render_current_subtask in control_flow_gridworld

--- gridworld_env/control_flow_gridworld.py
+++ gridworld_env/control_flow_gridworld.py
@@ -35,12 +35,6 @@ class ControlFlowGridWorld(SubtasksGridWorld):
             )._asdict()
         )
 
-    def render_current_subtask(self):
-        # if self.subtask_idx == 0:
-        # print("none")
-        # else:
-        super().render_current_subtask()
-
     def render_task(self):
         def helper(i, indent):
             try:
