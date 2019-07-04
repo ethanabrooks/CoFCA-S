97dc0c6fc42aec476ad58436f4b77e00371adc62
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-03 20:02:57 -0400

    learn phi_shift

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -74,13 +74,12 @@ class Recurrence(ppo.subtasks.agent.Recurrence):
         false_path = self.branch_one_hots[false_path.squeeze(-1).long()]
 
         def update_attention(p, t):
-            # c = conditions[torch.arange(N), inputs.subtask[t].long().flatten()]
-            c = (p.unsqueeze(1) @ conditions).squeeze(1)
-            phi_in = inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
-            truth = torch.any(phi_in.view(N, -1) > 0, dim=-1).float().view(N, 1, 1)
-            pred = truth
             # c = (p.unsqueeze(1) @ conditions).squeeze(1)
-            # pred = self.phi_shift((inputs.base[t], c))
+            # phi_in = inputs.base[t, :, 1:-2] * c.view(N, conditions.size(2), 1, 1)
+            # truth = torch.any(phi_in.view(N, -1) > 0, dim=-1).float().view(N, 1, 1)
+            # pred = truth
+            c = (p.unsqueeze(1) @ conditions).squeeze(1)
+            pred = self.phi_shift((inputs.base[t], c))
             trans = pred * true_path + (1 - pred) * false_path
             return (p.unsqueeze(1) @ trans).squeeze(1)
 
