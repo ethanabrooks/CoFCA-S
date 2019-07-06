aa435194add2b96204e05185370a20de36b62319
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-05 21:08:32 -0400

    use -0.1 rewards

--- ppo/subtasks/wrappers.py
+++ ppo/subtasks/wrappers.py
@@ -27,7 +27,7 @@ class DebugWrapper(gym.Wrapper):
         # print("guess", guess)
         r = 0
         if self.env.unwrapped.subtask is not None and self.guess != self.truth:
-            r = -1
+            r = -0.1
         s, _, t, i = super().step(action)
         self.last_reward = r
         return s, r, t, i
