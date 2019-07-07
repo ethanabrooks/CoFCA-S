0ddc1262a6c0adb7a9597900997639395cada5d7
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-07 11:18:38 -0400

    c=truth

--- ppo/subtasks/agent.py
+++ ppo/subtasks/agent.py
@@ -443,7 +443,7 @@ class Recurrence(torch.jit.ScriptModule):
                 else:
                     c = torch.sigmoid(c_logits[:, :1])
                     probs = torch.zeros_like(c_logits)  # dummy value
-                return c, probs
+                return truth, probs
 
             # cr
             cr, cr_probs = phi_update(subtask_param=r)
