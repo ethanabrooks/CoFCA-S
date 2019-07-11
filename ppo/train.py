302ce1672d98fe5e29df711ea2b6c1c79a7e4da8
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-11 13:50:36 -0400

    fix isort line length

--- ppo/train.py
+++ ppo/train.py
@@ -20,8 +20,14 @@ from ppo.agent import Agent, AgentValues  # noqa
 from ppo.storage import RolloutStorage
 from ppo.subtasks.wrappers import Wrapper
 from ppo.update import PPO
+from ppo.wrappers import (
+    AddTimestep,
+    TransposeImage,
+    VecNormalize,
+    VecPyTorch,
+    VecPyTorchFrameStack,
+)
 from ppo.utils import get_n_gpu, get_random_gpu
-from ppo.wrappers import AddTimestep, TransposeImage, VecNormalize, VecPyTorch, VecPyTorchFrameStack
 
 try:
     import dm_control2gym
