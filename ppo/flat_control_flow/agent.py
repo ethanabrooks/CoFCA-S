d81060424e969ebd495b0309554211078bfa20bc
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-11 11:03:56 -0400

    transition to sigmoid after feeding is_subtask and pred to networks

--- ppo/flat_control_flow/agent.py
+++ ppo/flat_control_flow/agent.py
@@ -76,11 +76,11 @@ class Recurrence(ppo.control_flow.agent.Recurrence):
             condition = r[:, -i:].view(N, i, 1, 1)
             obs = inputs.base[t, :, 1:-2]
             is_subtask = condition[:, 0]
-            # is_subtask = self.phi_shift2(condition[:, 0])
+            is_subtask = self.phi_shift2(condition[:, 0])
             pred = ((condition[:, 1:] * obs) > 0).view(N, 1, 1, -1).any(dim=-1).float()
             # pred = self.phi_shift((inputs.base[t], r))
-            # pred = self.phi_shift(pred)
-            take_one_step = torch.clamp(is_subtask + pred, 0.0, 1.0)
+            pred = self.phi_shift(pred)
+            take_one_step = torch.sigmoid(is_subtask + pred)
             take_two_steps = 1 - take_one_step
             trans = take_one_step * self.one_step + take_two_steps * self.two_steps
             return (p.unsqueeze(1) @ trans).squeeze(1)
