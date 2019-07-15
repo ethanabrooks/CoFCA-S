273eb55e004a48db319e12b94d35fccaa28832f7
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-15 14:24:20 -0400

    fully learn c

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -390,24 +390,24 @@ class Recurrence(torch.jit.ScriptModule):
                     probs = torch.zeros_like(c_logits)  # dummy value
 
                 # NOTE {
-                _task_sections = torch.split(
-                    subtask_param, tuple(self.subtask_nvec), dim=-1
-                )
-                interaction, count, obj, _, condition = _task_sections
-                agent_layer = obs[t, :, 6, :, :].long()
-                j, k, l = torch.split(agent_layer.nonzero(), 1, dim=-1)
-                debug_obs = obs[t, j, :, k, l].squeeze(1)
-                a_one_hot = self.a_one_hots[A[t]]
-                correct_object = obj * debug_obs[:, 1 : 1 + self.subtask_nvec[2]]
-                column1 = interaction[:, :1]
-                column2 = interaction[:, 1:] * a_one_hot[:, 4:-1]
-                correct_action = torch.cat([column1, column2], dim=-1)
-                c = (
-                    correct_action.sum(-1, keepdim=True)
-                    * correct_object.sum(-1, keepdim=True)
-                ).detach()
-                c = self.phi_debug(c)
-                assert torch.all(0 <= c) and torch.all(c <= 1)
+                # _task_sections = torch.split(
+                # subtask_param, tuple(self.subtask_nvec), dim=-1
+                # )
+                # interaction, count, obj, _, condition = _task_sections
+                # agent_layer = obs[t, :, 6, :, :].long()
+                # j, k, l = torch.split(agent_layer.nonzero(), 1, dim=-1)
+                # debug_obs = obs[t, j, :, k, l].squeeze(1)
+                # a_one_hot = self.a_one_hots[A[t]]
+                # correct_object = obj * debug_obs[:, 1 : 1 + self.subtask_nvec[2]]
+                # column1 = interaction[:, :1]
+                # column2 = interaction[:, 1:] * a_one_hot[:, 4:-1]
+                # correct_action = torch.cat([column1, column2], dim=-1)
+                # c = (
+                # correct_action.sum(-1, keepdim=True)
+                # * correct_object.sum(-1, keepdim=True)
+                # ).detach()
+                # c = self.phi_debug(c)
+                # assert torch.all(0 <= c) and torch.all(c <= 1)
                 # NOTE }
                 return c, probs
 
