4782a78b2863b3689cc1ac36e349a508ae346775
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-16 08:54:51 -0400

    now train if-else-subtask

--- gridworld_env/control_flow_gridworld.py
+++ gridworld_env/control_flow_gridworld.py
@@ -220,25 +220,36 @@ class ControlFlowGridworld(SubtasksGridworld):
     #             raise RuntimeError
 
     def subtasks_generator(self):
-        # yield self.If(None)
+        yield self.If(None)
+        yield self.Subtask(
+            interaction=self.np_random.choice(self.irreversible_interactions),
+            count=0,
+            object=None,
+        )
+        yield Else()
+        yield self.Subtask(
+            interaction=self.np_random.choice(self.irreversible_interactions),
+            count=0,
+            object=None,
+        )
+        yield EndIf()
+        yield self.Subtask(
+            interaction=self.np_random.choice(len(self.interactions)),
+            count=0,
+            object=None,
+        )
+        # yield self.While(None)
         # yield self.Subtask(
         #     interaction=self.np_random.choice(self.irreversible_interactions),
         #     count=0,
         #     object=None,
         # )
-        # yield EndIf()
-        yield self.While(None)
-        yield self.Subtask(
-            interaction=self.np_random.choice(self.irreversible_interactions),
-            count=0,
-            object=None,
-        )
-        yield EndWhile()
-        yield self.Subtask(
-            interaction=self.np_random.choice(self.irreversible_interactions),
-            count=0,
-            object=None,
-        )
+        # yield EndWhile()
+        # yield self.Subtask(
+        #     interaction=self.np_random.choice(self.irreversible_interactions),
+        #     count=0,
+        #     object=None,
+        # )
 
     def get_required_objects(self, subtasks):
         available = []
