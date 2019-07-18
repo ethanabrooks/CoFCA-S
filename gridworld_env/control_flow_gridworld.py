de5dcd6ef4b4ad6f8dd3b649f7ef23825aa03e69
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-18 11:33:05 -0400

    Revert "add third subtask (second after if-block) to prevent else -> if"
    This reverts commit e05254e6083a0aa8b8fa8e626a60aae8f1bc5d5b.

--- gridworld_env/control_flow_gridworld.py
+++ gridworld_env/control_flow_gridworld.py
@@ -238,7 +238,7 @@ class ControlFlowGridworld(SubtasksGridworld):
     #             raise RuntimeError
 
     def subtasks_generator(self):
-        assert self.n_subtasks == 5
+        assert self.n_subtasks == 4
         self.np_random.shuffle(self.irreversible_interactions)
         if self.np_random.rand() < 1:  # TODO
             yield self.If(None)
@@ -258,9 +258,6 @@ class ControlFlowGridworld(SubtasksGridworld):
             yield EndIf()
             yield self.Subtask(
                 interaction=self.irreversible_interactions[1], count=0, object=None
-            )
-            yield self.Subtask(
-                interaction=self.irreversible_interactions[0], count=0, object=None
             )
         else:
             yield self.While(None)
