270b362fe194b8688d213fb638bf1a999320076f
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-18 11:33:26 -0400

    assert self.n_sutbasks = 5

--- gridworld_env/control_flow_gridworld.py
+++ gridworld_env/control_flow_gridworld.py
@@ -238,7 +238,7 @@ class ControlFlowGridworld(SubtasksGridworld):
     #             raise RuntimeError
 
     def subtasks_generator(self):
-        assert self.n_subtasks == 4
+        assert self.n_subtasks == 5
         self.np_random.shuffle(self.irreversible_interactions)
         if self.np_random.rand() < 1:  # TODO
             yield self.If(None)
