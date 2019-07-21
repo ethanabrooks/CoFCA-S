bd46f57fda30890587af4c8e63bed4e460fedf69
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-21 08:01:48 -0400

    clip n_random between min_objects and len(open_spaces)

--- gridworld_env/subtasks_gridworld.py
+++ gridworld_env/subtasks_gridworld.py
@@ -227,17 +227,19 @@ class SubtasksGridworld(gym.Env):
 
         if self.random_task:
             self.subtasks = list(self.subtasks_generator())
+
+        h, w = self.desc.shape
+        ij = cartesian_product(np.arange(h), np.arange(w))
+        open_spaces = ij[np.logical_not(np.all(np.isin(ij, self.obstacles), axis=-1))]
+
         types = list(self.get_required_objects(self.subtasks))
-        n_random = max(len(types), self.min_objects)
+        n_random = np.clip(len(types), self.min_objects, len(open_spaces))
         random_types = self.np_random.choice(
             len(self.object_types), replace=True, size=n_random - len(types)
         )
         types = np.concatenate([random_types, types])
         self.np_random.shuffle(types)
 
-        h, w = self.desc.shape
-        ij = cartesian_product(np.arange(h), np.arange(w))
-        open_spaces = ij[np.logical_not(np.all(np.isin(ij, self.obstacles), axis=-1))]
         randoms = self.np_random.choice(
             len(open_spaces), replace=False, size=n_random + 1  # + 1 for agent
         )
