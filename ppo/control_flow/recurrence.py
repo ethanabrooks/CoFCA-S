958d0b5ac2edd925a3cde0879c9b7cc53d149b35
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-26 14:04:43 -0400

    disable xi_debug

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -381,17 +381,17 @@ class Recurrence(torch.jit.ScriptModule):
             l = self.xi((inputs.base[t], condition))
             self.print("l", round(l, 4))
             # NOTE {
-            c = torch.split(condition, list(self.subtask_nvec), dim=-1)[-1][:, 1:]
-            last_condition = torch.split(
-                hx.last_condition, list(self.subtask_nvec), dim=-1
-            )[-1][:, 1:]
-            hx_r = torch.split(hx.r, list(self.subtask_nvec), dim=-1)[-1][:, 1:]
-            self.print("last_condition", last_condition)
-            self.print("r", hx_r)
-            self.print("l condition", c)
-            phi_in = inputs.base[t, :, 1:-2] * c.view(N, -1, 1, 1)
-            truth = torch.max(phi_in.view(N, -1), dim=-1).values.float().view(N, 1)
-            l = self.xi_debug((inputs.base[t, :, 1:-2], c))
+            # c = torch.split(condition, list(self.subtask_nvec), dim=-1)[-1][:, 1:]
+            # last_condition = torch.split(
+            # hx.last_condition, list(self.subtask_nvec), dim=-1
+            # )[-1][:, 1:]
+            # hx_r = torch.split(hx.r, list(self.subtask_nvec), dim=-1)[-1][:, 1:]
+            # self.print("last_condition", last_condition)
+            # self.print("r", hx_r)
+            # self.print("l condition", c)
+            # phi_in = inputs.base[t, :, 1:-2] * c.view(N, -1, 1, 1)
+            # truth = torch.max(phi_in.view(N, -1), dim=-1).values.float().view(N, 1)
+            # l = self.xi_debug((inputs.base[t, :, 1:-2], c))
 
             # self.print("l truth", round(truth, 4))
             # l = truth
