b205a4f6ee1e0af038bff928584efcc49ea75ec8
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-03 21:18:58 -0400

    cast action to int

--- ppo/events/gridworld.py
+++ ppo/events/gridworld.py
@@ -113,7 +113,7 @@ class Gridworld(gym.Env):
 
     def step(self, a):
         self.last_action = a
-        action = self.transitions[a]
+        action = self.transitions[int(a)]
         interactions = list(self.interact()) if action == (0, 0) else []
 
         obj: Object
