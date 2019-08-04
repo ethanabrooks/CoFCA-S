48e9dc154a3f021296c2a24406ce62a17c09ebb8
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-04 13:35:59 -0400

    breakpoints

--- ppo/train.py
+++ ppo/train.py
@@ -136,9 +136,14 @@ class Train:
             rollouts.to(device)
             print("Values copied to GPU in", time.time() - tick, "seconds")
 
+        p0 = list(self.agent.parameters())[0]
         if compare_path:
             with Path(compare_path, "parameters").open("rb") as f:
                 params = pickle.load(f)
+                _p0 = list(params)[0]
+                import ipdb
+
+                ipdb.set_trace()
                 for p1, p2 in zip(params, self.agent.parameters()):
                     if not torch.all(p1 == p2):
                         import ipdb
@@ -148,6 +153,9 @@ class Train:
         else:
             with Path(log_dir, "parameters").open("wb") as f:
                 pickle.dump(list(self.agent.parameters()), f)
+        import ipdb
+
+        ipdb.set_trace()
 
         ppo = PPO(agent=self.agent, batch_size=batch_size, **ppo_args)
 
