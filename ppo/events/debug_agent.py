a81021baedfddecd2534e380afe752c6b49fd8c9
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-05 12:20:43 -0400

    move sampling into debug Recurrence

--- ppo/events/debug_agent.py
+++ ppo/events/debug_agent.py
@@ -3,8 +3,9 @@ from gym.spaces import Box, Discrete
 from torch import nn as nn
 
 from ppo.agent import AgentValues
-from ppo.distributions import Categorical
+from ppo.distributions import Categorical, FixedCategorical
 from ppo.distributions import DiagGaussian
+from ppo.events.recurrence import RecurrentState
 from ppo.layers import Flatten
 from ppo.utils import init_
 
@@ -47,21 +48,13 @@ class DebugAgent(nn.Module):
         return self.base.is_recurrent
 
     def forward(self, inputs, rnn_hxs, masks, deterministic=False, action=None):
-        value, actor_features, rnn_hxs = self.base(inputs, rnn_hxs, masks)
-
-        dist = self.dist(actor_features)
-
-        if action is None:
-            if deterministic:
-                action = dist.mode()
-            else:
-                action = dist.sample()
-
-        action_log_probs = dist.log_probs(action)
+        hx = self.base(inputs, rnn_hxs, masks, action=action)
+        dist = FixedCategorical(hx.a_probs)
+        action_log_probs = dist.log_probs(hx.a)
         entropy = dist.entropy().mean()
         return AgentValues(
-            value=value,
-            action=action,
+            value=hx.v,
+            action=hx.a,
             action_log_probs=action_log_probs,
             aux_loss=-self.entropy_coef * entropy,
             dist=dist,
@@ -70,69 +63,14 @@ class DebugAgent(nn.Module):
         )
 
     def get_value(self, inputs, rnn_hxs, masks):
-        value, _, _ = self.base(inputs, rnn_hxs, masks)
-        return value
+        return self.base(inputs, rnn_hxs, masks).v
 
 
 class Recurrence(nn.Module):
-    def _forward_gru(self, x, hxs, masks):
-        if x.size(0) == hxs.size(0):
-            x, hxs = self.recurrent_module(x.unsqueeze(0), (hxs * masks).unsqueeze(0))
-            x = x.squeeze(0)
-            hxs = hxs.squeeze(0)
-        else:
-            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
-            N = hxs.size(0)
-            T = int(x.size(0) / N)
-
-            # unflatten
-            x = x.view(T, N, *x.shape[1:])
-
-            # Same deal with masks
-            masks = masks.view(T, N)
-
-            # Let's figure out which steps in the sequence have a zero for any agent
-            # We will always assume t=0 has a zero in it as that makes the logic cleaner
-            has_zeros = (masks[1:] == 0.0).any(dim=-1).nonzero().squeeze().cpu()
-
-            # +1 to correct the masks[1:]
-            if has_zeros.dim() == 0:
-                # Deal with scalar
-                has_zeros = [has_zeros.item() + 1]
-            else:
-                has_zeros = (has_zeros + 1).numpy().tolist()
-
-            # add t=0 and t=T to the list
-            has_zeros = [0] + has_zeros + [T]
-
-            hxs = hxs.unsqueeze(0)
-            outputs = []
-            for i in range(len(has_zeros) - 1):
-                # We can now process steps that don't have any zeros in masks together!
-                # This is much faster
-                start_idx = has_zeros[i]
-                end_idx = has_zeros[i + 1]
-
-                rnn_scores, hxs = self.recurrent_module(
-                    x[start_idx:end_idx], hxs * masks[start_idx].view(1, -1, 1)
-                )
-
-                outputs.append(rnn_scores)
-
-            # assert len(outputs) == T
-            # x is a (T, N, -1) tensor
-            x = torch.cat(outputs, dim=0)
-            # flatten
-            x = x.view(T * N, -1)
-            hxs = hxs.squeeze(0)
-
-        return x, hxs
-
     def __init__(self, d, h, w, activation, hidden_size, num_layers, recurrent=False):
         super().__init__()
         self._hidden_size = hidden_size
         self._recurrent = recurrent
-
         self.f = nn.Sequential(
             init_(nn.Conv2d(d, hidden_size, kernel_size=1)),
             activation,
@@ -151,11 +89,19 @@ class Recurrence(nn.Module):
             activation,
         )
         self.critic = init_(nn.Linear(hidden_size, 1))
+        self.actor = Categorical(self.output_size, 5)
         self.train()
 
-    def forward(self, inputs, rnn_hxs, masks):
+    def forward(self, inputs, rnn_hxs, masks, action=None):
         s = self.f(inputs)
-        return self.critic(s), s, rnn_hxs
+        dist = self.actor(s)
+
+        if action is None:
+            action = dist.sample()
+
+        return RecurrentState(
+            a=action, a_probs=dist.probs, v=self.critic(s), s=s, p=None
+        )
 
     @property
     def recurrent_hidden_state_size(self):
