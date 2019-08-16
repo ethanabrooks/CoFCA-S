4f18400105e2fddc08f202eebeae323d5a598074
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-16 17:59:25 -0400

    install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
