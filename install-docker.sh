eda5c18c82fa2e60f966a674f1c47504d391e105
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-16 18:16:29 -0400

    install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -4,4 +4,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
 sudo apt update
 sudo apt install -y docker-ce
+sudo usermod -a -G docker $USER
 sudo docker build --tag=ppo .
