c5cdee413ae3af8c900aacb000802d00df726a98
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-16 18:02:01 -0400

    install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+sudo apt update
+sudo apt install -y docker-ce
