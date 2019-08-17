c423528800b08d55e20ea3ee44265bfe615b4710
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-16 21:24:32 -0400

    fix tune.sh

--- tune.sh
+++ tune.sh
@@ -1,31 +1,28 @@
-nice exp \
+nice python ppo/main.py \
   --cuda-deterministic \
-  --log-dir=/home/ethanbro/tune_results
+  --log-dir=/home/ethanbro/tune_results \
   --run-id=tune/maiden \
   --num-processes="300" \
-
+  \
   --tune \
   --redis-address 141.212.113.250:6379 \
-
-  # env variables
+  \
   --subtask="AnswerDoor" \
   --subtask="AvoidDog" \
   --subtask="ComfortBaby" \
   --subtask="KillFlies" \
   --subtask="MakeFire" \
   --subtask="WatchBaby" \
-  --test "WatchBaby"  "KillFlies" "MakeFire"\
-  --test "AnswerDoor"  "KillFlies" "AvoidDog"\
-  --test "AnswerDoor"  "MakeFire" "AvoidDog"\
+  --test "WatchBaby"  "KillFlies" "MakeFire" \
+  --test "AnswerDoor"  "KillFlies" "AvoidDog" \
+  --test "AnswerDoor"  "MakeFire" "AvoidDog" \
   --n-active-subtasks="3" \
   --time-limit="30" \
-
-  # intervals
+  \
   --eval-interval="100" \
   --log-interval="10" \
   --save-interval="300" \
-
-  # dummies
+  \
   --env="" \
   --num-batch="-1" \
   --num-steps="-1" \
