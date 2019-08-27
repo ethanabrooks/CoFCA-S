48138ff5dd90d38c161e651b0b4ddfa678024d0d
Ethan Brooks <ethanabrooks@gmail.com>
2019-08-27 12:01:37 -0400

    record the tick that fps is computed with

--- ppo/train.py
+++ ppo/train.py
@@ -231,7 +231,6 @@ class Train(abc.ABC):
                 total_num_steps = (self.i + 1) * num_processes * num_steps
                 # print(f"Writing to {self.logdir}")
                 fps = total_num_steps / (time.time() - tick)
-                tick = time.time()
                 yield dict(
                     k_scalar_pairs(
                         tick=tick,
@@ -241,6 +240,7 @@ class Train(abc.ABC):
                         **eval_result,
                     )
                 )
+                tick = time.time()
 
     def run_epoch(
         self, obs, rnn_hxs, masks, num_steps, counter, success_reward, use_tqdm
