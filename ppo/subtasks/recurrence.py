80b8a3ac27dca295714b486e761dbc477de63560
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-05 17:49:23 -0400

    got both baseline and not baselin running

--- ppo/subtasks/recurrence.py
+++ ppo/subtasks/recurrence.py
@@ -11,7 +11,7 @@ from ppo.distributions import FixedCategorical
 from ppo.layers import Concat
 from ppo.utils import init_
 
-RecurrentState = namedtuple("RecurrentState", "a v h a_probs")
+RecurrentState = namedtuple("RecurrentState", "a v h a_probs p")
 
 
 def batch_conv1d(inputs, weights):
@@ -71,8 +71,10 @@ class Recurrence(nn.Module):
             actor_out = hidden_size
             action_n = action_space.n
         self.actor = nn.Linear(hidden_size, actor_out)
-        self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(action_n))
-        self.state_sizes = RecurrentState(a=1, a_probs=action_n, v=1, h=hidden_size)
+        self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(action_space.n))
+        self.state_sizes = RecurrentState(
+            a=1, a_probs=action_n, p=action_space.n, v=1, h=hidden_size
+        )
 
     @staticmethod
     def sample_new(x, dist):
@@ -144,15 +146,14 @@ class Recurrence(nn.Module):
             _x.squeeze_(0)
 
         h = hx.h
-        # p[new_episode] = 0
+        p = hx.p
+        p[new_episode, 0] = 1
         A = torch.cat([actions, hx.a.unsqueeze(0)], dim=0).long().squeeze(2)
 
         for t in range(T):
-            a = self.a_one_hots(A[t - 1]).unsqueeze(1)
-            r = (a @ M).squeeze(1)
+            r = (p.unsqueeze(1) @ M).squeeze(1)
             h = self.gru(self.f((inputs.condition[t], r)), h)
             k = self.actor(h)
-            # w = F.cosine_similarity(K, k.unsqueeze(1), dim=2) # TODO: try this
             if self.baseline:
                 l, no_op = torch.split(k, [3, 1], dim=-1)
                 l = F.softmax(l, dim=-1)
@@ -161,20 +162,28 @@ class Recurrence(nn.Module):
                 no_op = torch.sigmoid(no_op)
                 self.print("no op")
                 self.print(no_op)
-                probs = batch_conv1d(a.squeeze(1), l)
+                probs = batch_conv1d(p.squeeze(1), l)
                 self.print("probs1")
                 self.print(probs)
                 probs = torch.cat([probs * (1 - no_op), no_op], dim=-1)
                 self.print("probs2")
                 self.print(probs)
                 dist = FixedCategorical(probs=probs / probs.sum(-1, keepdim=True))
+                self.print("dist")
+                self.print(dist.probs)
+                self.sample_new(A[t], dist)
+                new = A[t] < self.state_sizes.p
+                a = A[t] * new.long()
+                new = new.float().unsqueeze(1)
+                p = self.a_one_hots(a) * new + p * (1 - new)
             else:
-                c = (a @ C).squeeze(1)
+                c = (p.unsqueeze(1) @ C).squeeze(1)
                 w = (K @ k.unsqueeze(2)).squeeze(2)
                 self.print("w")
                 self.print(w)
                 dist = FixedCategorical(logits=w * c)
-            self.print("dist")
-            self.print(dist.probs)
-            self.sample_new(A[t], dist)
-            yield RecurrentState(a=A[t], v=self.critic(h), h=h, a_probs=dist.probs)
+                self.print("dist")
+                self.print(dist.probs)
+                self.sample_new(A[t], dist)
+                p = self.a_one_hots(A[t])
+            yield RecurrentState(a=A[t], v=self.critic(h), h=h, a_probs=dist.probs, p=p)
