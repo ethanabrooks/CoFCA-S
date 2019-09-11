2a183e66b7587f499a92881ff99097e00e264799
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-11 10:00:40 -0400

    base actions on estimated value

--- ppo/values/recurrence.py
+++ ppo/values/recurrence.py
@@ -50,17 +50,24 @@ class Recurrence(nn.Module):
         self.action_size = 1
         self.debug = debug
         self.hidden_size = hidden_size
+        self.na = action_space.n
 
         # networks
-        # self.S = nn.Parameter(torch.normal(0, 1, (int(h * w), hidden_size)))
-        # self.A = nn.Parameter(torch.normal(0, 1, (int(action_space.n), hidden_size)))
-        self.S = nn.Parameter(torch.normal(0, 1, size=(h * w, hidden_size)))
-        self.S1 = nn.Parameter(torch.normal(0, 1, size=(h * w, hidden_size)))
-        self.A = nn.Parameter(
-            torch.normal(0, 1, size=(int(action_space.n), hidden_size))
-        )
-        self.SA = nn.Parameter(self.S.unsqueeze(-1) + self.A.T.unsqueeze(0))
-        self.T = nn.Parameter(self.S @ self.SA)
+        layers = [Concat(dim=-1)]
+        in_size = hidden_size * 2
+        for i in range(num_layers):
+            layers += [init_(nn.Linear(in_size, hidden_size)), activation]
+            in_size = hidden_size
+        self.f = nn.Sequential(*layers)
+        self.actor = Categorical(in_size, self.na)
+        self.critic = init_(nn.Linear(in_size, 1))
+        self.register_buffer("state_indexer", torch.tensor([w, 1.0]))
+        self.S = nn.Embedding(h * w, hidden_size)
+        _S = nn.Parameter(torch.normal(0, 1, size=(h * w, hidden_size)))
+        S = self.S.weight
+        A = nn.Parameter(torch.normal(0, 1, size=(int(action_space.n), hidden_size)))
+        SA = nn.Parameter(S.unsqueeze(-1) + A.T.unsqueeze(0))
+        self.T = nn.Parameter(S @ SA)
         # SA = nn.Parameter(
         #     torch.normal(0, 1, size=(h * w, hidden_size, int(action_space.n)))
         # )
@@ -70,7 +77,7 @@ class Recurrence(nn.Module):
         # )
         self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(int(h * w)))
         self.state_sizes = RecurrentState(
-            a=1, a_probs=action_space.n, v=1, h=hidden_size, estimated_values=h * w
+            a=1, a_probs=action_space.n, v=1, h=in_size, estimated_values=h * w
         )
 
     @staticmethod
@@ -103,23 +110,35 @@ class Recurrence(nn.Module):
 
     def inner_loop(self, inputs, rnn_hxs):
         T, N, D = inputs.shape
-        ns = self.S.size(0)
-        na = self.A.size(0)
+        ns = self.S.num_embeddings
+        na = self.na
         obs, action = torch.split(inputs.detach(), [D - 1, 1], dim=-1)
         obs = self.parse_inputs(obs)
         hx = self.parse_hidden(rnn_hxs)
+        for _x in hx:
+            _x.squeeze_(0)
 
+        A = torch.cat([action, hx.a.unsqueeze(0)], dim=0).long().squeeze(2)
         for t in range(T):
             values = torch.zeros_like(obs.rewards[t])
             for _ in range(self.time_limit):
                 P = self.T.softmax(dim=1)
                 EV = (values @ P.view(ns, -1)).view(N, ns, na)
-                values = obs.rewards[t] + EV.max(dim=-1).values
+                maxEV = EV.max(dim=-1)
+                values = obs.rewards[t] + maxEV.values
 
+            state_idx = (obs.state[t] @ self.state_indexer).long()
+            next_state_idx = maxEV.indices.gather(dim=-1, index=state_idx.unsqueeze(-1))
+            s1 = self.S(state_idx)
+            s2 = self.S(next_state_idx).squeeze(1)
+            h = self.f((s1, s2))
+
+            dist = self.actor(h)
+            self.sample_new(A[t], dist)
             yield RecurrentState(
-                a=hx.a[t],
-                a_probs=hx.a_probs[t],
-                v=hx.v[t],
-                h=hx.h[t],
+                a=A[t],
+                a_probs=dist.probs,
+                v=self.critic(h),
+                h=h,
                 estimated_values=values,
             )
