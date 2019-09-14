fe55ab96fe223dd03caf1826653e8928ef724388
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-14 19:30:59 -0400

    wr=cr

--- ppo/blocks_world/recurrence.py
+++ ppo/blocks_world/recurrence.py
@@ -189,7 +189,6 @@ class Recurrence(nn.Module):
             ww2 = ww.unsqueeze(-2)
             M = M * (1 - ww1 * e) + ww1 * v
             # page 7 right column
-            """
 
             # read
             p = (1 - ww.sum(-1, keepdim=True)) * p + ww
@@ -198,7 +197,6 @@ class Recurrence(nn.Module):
             L = (1 - self.mem_one_hots).unsqueeze(0) * L  # zero out L[i, i]
             b = wr @ L
             f = wr @ L.transpose(1, 2)
-            """
             Kr = Kr.view(N, self.num_heads, 1, self.slot_size)
             cr = (
                 br.unsqueeze(-1) * F.cosine_similarity(M.unsqueeze(1), Kr, dim=-1)
