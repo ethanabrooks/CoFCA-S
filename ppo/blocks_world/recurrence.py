c25555f1bb0501d6f36c0842353c21d071fa58a1
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-15 10:44:45 -0400

    feed inputs, r, and h into gru. Feed h and r into actor/critic

--- ppo/blocks_world/recurrence.py
+++ ppo/blocks_world/recurrence.py
@@ -10,7 +10,7 @@ from ppo.layers import Concat
 from ppo.mdp.env import Obs
 from ppo.utils import init_
 
-RecurrentState = namedtuple("RecurrentState", "a a_probs v r wr u ww M p L")
+RecurrentState = namedtuple("RecurrentState", "a a_probs v h r wr u ww M p L")
 XiSections = namedtuple("XiSections", "Kr Br kw bw e v F_hat ga gw Pi")
 
 
@@ -56,6 +56,7 @@ class Recurrence(nn.Module):
             a=1,
             a_probs=action_space.n,
             v=1,
+            h=num_layers * hidden_size,
             r=num_heads * slot_size,
             wr=num_heads * num_slots,
             u=num_slots,
@@ -80,18 +81,20 @@ class Recurrence(nn.Module):
         # networks
         assert num_layers > 0
         self.gru = nn.GRU(
-            int(embedding_size * np.prod(nvec.shape)), hidden_size, num_layers
+            int(embedding_size * np.prod(nvec.shape)) + num_heads * slot_size,
+            hidden_size,
+            num_layers,
         )
-        self.f1 = nn.Sequential(
-            init_(nn.Linear(num_heads * slot_size, num_layers * hidden_size)),
-            activation,
-        )
-        self.f2 = nn.Sequential(
+        self.Wxi = nn.Sequential(
             activation,
             init_(nn.Linear(num_layers * hidden_size, sum(self.xi_sections))),
         )
-        self.actor = Categorical(num_heads * slot_size, action_space.n)
-        self.critic = init_(nn.Linear(num_heads * slot_size, 1))
+        self.actor = Categorical(
+            num_layers * hidden_size + num_heads * slot_size, action_space.n
+        )
+        self.critic = init_(
+            nn.Linear(num_layers * hidden_size + num_heads * slot_size, 1)
+        )
 
         self.register_buffer("mem_one_hots", torch.eye(num_slots))
         self.embeddings = nn.Embedding(int(nvec.max()), embedding_size)
@@ -145,21 +148,23 @@ class Recurrence(nn.Module):
         M = hx.M.view(N, self.num_slots, self.slot_size)
         p = hx.p
         L = hx.L.view(N, self.num_slots, self.num_slots)
+        h = (
+            hx.h.view(N, self.gru.num_layers, self.gru.hidden_size)
+            .transpose(0, 1)
+            .contiguous()
+        )
 
         A = torch.cat([actions, hx.a.unsqueeze(0)], dim=0).long()
 
         for t in range(T):
-            h = self.f1(r.view(N, -1))
-            _, h = self.gru(
-                inputs[t].view(1, N, -1),
-                h.view(N, self.gru.num_layers, self.gru.hidden_size)
-                .transpose(0, 1)
-                .contiguous(),
-            )
-            xi = self.f2(h.view(N, -1))
+            x = torch.cat([inputs[t].view(1, N, -1), r.view(1, N, -1)], dim=-1)
+            _, h = self.gru(x, h)
+            hT = h.transpose(0, 1).reshape(N, -1)  # switch layer and batch dims
+            xi = self.Wxi(hT)
             Kr, br, kw, bw, e, v, free, ga, gw, Pi = xi.squeeze(0).split(
                 self.xi_sections, dim=-1
             )
+            # preprocess
             br = F.softplus(br.view(N, self.num_heads))
             bw = F.softplus(bw)
             e = e.sigmoid().view(N, 1, self.slot_size)
@@ -202,16 +207,19 @@ class Recurrence(nn.Module):
                 br.unsqueeze(-1) * F.cosine_similarity(M.unsqueeze(1), Kr, dim=-1)
             ).softmax(-1)
             wr = Pi[0] * b + Pi[1] * cr + Pi[2] * f
-            r = wr @ M
+            r = (wr @ M).view(N, -1)
 
             # act
-            dist = self.actor(r.view(N, -1))
-            value = self.critic(r.view(N, -1))
+            x = torch.cat([hT, r], dim=-1)
+            dist = self.actor(x)  # page 7 left column
+            value = self.critic(x)
             self.sample_new(A[t], dist)
+
             yield RecurrentState(
                 a=A[t],
                 a_probs=dist.probs,
                 v=value,
+                h=hT,
                 r=r,
                 wr=wr,
                 u=u,
