d2678b4ad8ca010989068406f2a6513c439767fb
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-19 12:03:05 -0400

    move copies of agent and recurrence into planner/

--- ppo/blocks_world/planner/recurrence.py
+++ ppo/blocks_world/planner/recurrence.py
@@ -2,23 +2,31 @@ from collections import namedtuple
 
 import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn as nn
+import torch.nn.functional as F
 
-from ppo.distributions import FixedCategorical
-from ppo.layers import Flatten
+from ppo.distributions import FixedCategorical, Categorical
+from ppo.layers import Concat
 from ppo.mdp.env import Obs
 from ppo.utils import init_
-import time
 
-RecurrentState = namedtuple(
-    # "RecurrentState", "values probs options indices model_loss embed_loss states h a v"
-    "RecurrentState",
-    "indices states model_loss log_probs entropy options values a v",
-)
-Actions = namedtuple("Actions", "actual options")
+RecurrentState = namedtuple("RecurrentState", "a a_probs v h r wr u ww M p L")
+XiSections = namedtuple("XiSections", "Kr Br kw bw e v F_hat ga gw Pi")
 
-INF = 1e8
+
+def batch_conv1d(inputs, weights):
+    outputs = []
+    # one convolution per instance
+    n = inputs.shape[0]
+    for i in range(n):
+        x = inputs[i]
+        w = weights[i]
+        convolved = F.conv1d(x.reshape(1, 1, -1), w.reshape(1, 1, -1), padding=2)
+        outputs.append(convolved.squeeze(0))
+    padded = torch.cat(outputs)
+    padded[:, 1] = padded[:, 1] + padded[:, 0]
+    padded[:, -2] = padded[:, -2] + padded[:, -1]
+    return padded[:, 1:-1]
 
 
 class Recurrence(nn.Module):
@@ -30,64 +38,69 @@ class Recurrence(nn.Module):
         hidden_size,
         num_layers,
         debug,
-        planning_steps,
+        num_slots,
+        slot_size,
         embedding_size,
+        num_heads,
+        planning_steps,
         num_model_layers,
         num_embedding_layers,
     ):
         super().__init__()
-        self.action_size = planning_steps + 1
+        self.action_size = 1
         self.debug = debug
+        self.slot_size = slot_size
+        self.num_slots = num_slots
+        self.num_heads = num_heads
         nvec = observation_space.nvec
         self.obs_shape = (*nvec.shape, nvec.max())
-        self.planning_steps = planning_steps
-        self.embedding_size = embedding_size
-        self.num_options = action_space.nvec.max().item()  # TODO: change
-        self.hidden_size = hidden_size
-        self.num_model_layers = num_model_layers
 
         self.state_sizes = RecurrentState(
-            values=planning_steps * self.num_options,
-            log_probs=1,
-            entropy=1,
-            options=planning_steps,
-            indices=planning_steps,
-            model_loss=1,
-            states=planning_steps * embedding_size,
             a=1,
+            a_probs=action_space.n,
             v=1,
+            h=num_layers * hidden_size,
+            r=num_heads * slot_size,
+            wr=num_heads * num_slots,
+            u=num_slots,
+            ww=num_slots,
+            M=num_slots * slot_size,
+            p=num_slots,
+            L=num_slots * num_slots,
         )
-        self.action_sections = Actions(actual=1, options=planning_steps)
+        self.xi_sections = XiSections(
+            Kr=num_heads * slot_size,
+            Br=num_heads,
+            kw=slot_size,
+            bw=1,
+            e=slot_size,
+            v=slot_size,
+            F_hat=num_heads,
+            ga=1,
+            gw=1,
+            Pi=3 * num_heads,
+        )
 
         # networks
-        self.embed_options = nn.Embedding(self.num_options, self.num_options)
+        assert num_layers > 0
+        self.gru = nn.GRU(
+            int(embedding_size * np.prod(nvec.shape)) + num_heads * slot_size,
+            hidden_size,
+            num_layers,
+        )
+        self.Wxi = nn.Sequential(
+            activation,
+            init_(nn.Linear(num_layers * hidden_size, sum(self.xi_sections))),
+        )
+        self.actor = Categorical(
+            num_layers * hidden_size + num_heads * slot_size, action_space.n
+        )
+        self.critic = init_(
+            nn.Linear(num_layers * hidden_size + num_heads * slot_size, 1)
+        )
 
-        layers = [nn.Embedding(nvec.max(), nvec.max()), Flatten()]
-        in_size = int(nvec.max() * np.prod(nvec.shape))
-        for _ in range(num_embedding_layers):
-            layers += [activation, init_(nn.Linear(in_size, hidden_size))]
-            in_size = hidden_size
-        self.embed1 = nn.Sequential(*layers)
-        self.embed2 = nn.Sequential(
-            activation, init_(nn.Linear(hidden_size, embedding_size))
-        )
-        self.model = nn.GRU(
-            embedding_size + self.num_options, hidden_size, num_model_layers
-        )
-        self.sharpener = nn.Sequential(
-            activation, init_(nn.Linear(embedding_size, 1)), nn.Softplus()
-        )
-        self.actor = nn.Sequential(
-            activation, init_(nn.Linear(hidden_size, self.num_options))
-        )
-        self.critic = nn.Sequential(activation, init_(nn.Linear(hidden_size, 1)))
-
-        self.logits_eye = nn.Embedding.from_pretrained(torch.eye(self.num_options))
-        self.register_buffer(
-            "eye", torch.eye(self.planning_steps).unsqueeze(0).unsqueeze(-1)
-        )
-        self.register_buffer("one", torch.tensor(1))
-        self.register_buffer("zero", torch.tensor(0))
+        self.register_buffer("mem_one_hots", torch.eye(num_slots))
+        self.embeddings = nn.Embedding(int(nvec.max()), embedding_size)
 
     @staticmethod
     def sample_new(x, dist):
@@ -115,9 +128,6 @@ class Recurrence(nn.Module):
     def parse_hidden(self, hx: torch.Tensor) -> RecurrentState:
         return RecurrentState(*torch.split(hx, self.state_sizes, dim=-1))
 
-    def parse_actions(self, actions):
-        return Actions(*torch.split(actions, self.action_sections, dim=-1))
-
     def print(self, t, *args, **kwargs):
         if self.debug:
             if type(t) == torch.Tensor:
@@ -126,132 +136,100 @@ class Recurrence(nn.Module):
 
     def inner_loop(self, inputs, rnn_hxs):
         T, N, D = inputs.shape
-        device = inputs.device
         inputs, actions = torch.split(
             inputs.detach(), [D - self.action_size, self.action_size], dim=2
         )
-        inputs = self.embed1(inputs.view(T * N, -1).long()).view(T, N, -1)
-        new_actions = (actions < 0).any()
-        if new_actions:
-            assert (actions < 0).all()
-        actions = self.parse_actions(actions)
+        inputs = self.embeddings(inputs.long())
+
+        # new_episode = torch.all(rnn_hxs == 0, dim=-1).squeeze(0)
         hx = self.parse_hidden(rnn_hxs)
         for _x in hx:
             _x.squeeze_(0)
-        I = torch.ones(N, device=device).bool()
 
-        """
-        options = hx.options.view(N, self.planning_steps).long()
-        values = hx.values.view(N, self.planning_steps, self.num_options)
-        log_probs = hx.log_probs.flatten()
-        entropy = hx.entropy.flatten()
-        indices = hx.indices.view(N, self.planning_steps).long()
-        states = hx.states.view(N, self.planning_steps, self.embedding_size)
+        wr = hx.wr.view(N, self.num_heads, self.num_slots)
+        u = hx.u
+        ww = hx.ww.view(N, self.num_slots)
+        r = hx.r.view(N, -1).unsqueeze(0)
+        M = hx.M.view(N, self.num_slots, self.slot_size)
+        p = hx.p
+        L = hx.L.view(N, self.num_slots, self.num_slots)
+        h = (
+            hx.h.view(N, self.gru.num_layers, self.gru.hidden_size)
+            .transpose(0, 1)
+            .contiguous()
+        )
 
-        new = torch.all(rnn_hxs == 0, dim=-1)
-        if new.any():
-            assert new.all()
-            options = (
-                actions.options.view(T, N, self.planning_steps)
-                .long()[0]
-                .split(1, dim=-1)
-            )
-
-            log_probs = torch.zeros_like(log_probs)
-            entropy = torch.zeros_like(entropy)
-            indices = torch.zeros_like(indices)
-            states = torch.zeros_like(states)
-
-            hidden_states = torch.zeros(
-                N,
-                self.planning_steps,
-                self.hidden_size,
-                self.num_model_layers,
-                device=device,
-            )
-            logits = torch.zeros_like(values)
-            new_state = self.embed2(inputs[0])
-            J = torch.zeros_like(I).long()
-
-            for j in range(self.planning_steps):
-                states = states + self.eye[:, j] * new_state.unsqueeze(1)
-                indices[I, J] = j
-                # TODO: prevent cycles?
-
-                x = states[I, J]
-
-                sharpness = self.sharpener(x)
-                v = self.actor(x)
-                new_logits = (logits[I, J] == 0).all(-1, keepdim=True)
-                l = torch.where(new_logits, sharpness * v, logits[I, J])
-
-                dist = FixedCategorical(logits=l)
-                P = options[j].squeeze(-1)
-                self.sample_new(P, dist)
-                log_probs = log_probs + dist.log_prob(P)
-
-                entropy = entropy + dist.entropy()
-                logits = logits + self.eye[:, j] * (
-                    l - INF * self.logits_eye(P)
-                ).unsqueeze(1)
-
-                # stack stuff
-                # pop = v[I, P] <= 0
-                # push = v[I, P] > 0
-                push = I  # TODO
-                # if push.any():
-                embedded_options = self.embed_options(P)
-                model_input = torch.cat([states[I, J], embedded_options], dim=-1)
-                h = hidden_states[I, J]
-                model_output, h = self.model(
-                    model_input.unsqueeze(0), h.permute(2, 0, 1).contiguous()
-                )
-                hidden_states = hidden_states + self.eye[:, j].unsqueeze(-1) * (
-                    h.permute(1, 2, 0).unsqueeze(1)
-                )
-                new_state = self.embed2(model_output[0]).where(
-                    push.unsqueeze(-1), states[:, j]
-                )
-                J = torch.min(J + 1, self.planning_steps * self.one - 1).where(push, J)
-                # J = torch.max(J - 1, self.zero).where(pop, J)
-
-            options = torch.cat(options, dim=-1)
-            # TODO: somehow add early termination
-            """
-
-        # TODO: add obs to recurrence
+        A = torch.cat([actions, hx.a.unsqueeze(0)], dim=0).long()
 
         for t in range(T):
-            # J = indices[:, t]
-            # option = options[I, J]
-            logits = self.actor(inputs[t])
-            dist = FixedCategorical(logits=logits)
-            option = actions.actual[t].long()
-            self.sample_new(option, dist)
-            # model_loss = F.mse_loss(
-            #     states[I, J], self.embed2(inputs[t]).detach(), reduction="none"
-            # ).mean(-1)
-
-            # TODO add gate (c) so that obs is not compared every turn
-
-            yield RecurrentState(
-                values=hx.values,
-                options=hx.options,
-                indices=hx.indices,
-                log_probs=dist.log_probs(option),
-                entropy=dist.entropy(),
-                a=option,
-                model_loss=hx.model_loss,
-                states=hx.states,
-                v=self.critic(inputs[t]),
+            x = torch.cat([inputs[t].view(1, N, -1), r.view(1, N, -1)], dim=-1)
+            _, h = self.gru(x, h)
+            hT = h.transpose(0, 1).reshape(N, -1)  # switch layer and batch dims
+            xi = self.Wxi(hT)
+            Kr, br, kw, bw, e, v, free, ga, gw, Pi = xi.squeeze(0).split(
+                self.xi_sections, dim=-1
+            )
+            # preprocess
+            br = F.softplus(br.view(N, self.num_heads))
+            bw = F.softplus(bw)
+            e = e.sigmoid().view(N, 1, self.slot_size)
+            v = v.view(N, 1, self.slot_size)
+            free = free.sigmoid()
+            ga = ga.sigmoid()
+            gw = gw.sigmoid()
+            Pi = (
+                Pi.view(N, self.num_heads, -1)
+                .permute(2, 0, 1)
+                .softmax(dim=0)
+                .unsqueeze(-1)
             )
 
+            # write
+            psi = (1 - free.unsqueeze(-1) * wr).prod(dim=1)  # page 8 left column
+            u = (u + (1 - u) * ww) * psi
+            phi = u.sort(dim=-1)
+            phi_prod = torch.cumprod(phi.values, dim=-1)
+            phi_prod = F.pad(phi_prod, [1, 0], value=1)[:, :-1]
+            unsorted_phi_prod = phi_prod.scatter(-1, phi.indices, phi_prod)
+            a = (1 - u) * unsorted_phi_prod  # page 8 left column
+            cw = (
+                bw * F.cosine_similarity(M, kw.view(N, 1, self.slot_size), dim=-1)
+            ).softmax(dim=-1)
+            ww = gw * (ga * a + (1 - ga) * cw)
+            ww1 = ww.unsqueeze(-1)
+            ww2 = ww.unsqueeze(-2)
+            M = M * (1 - ww1 * e) + ww1 * v
+            # page 7 right column
 
-"""
-Questions:
-- prevent cycles
-- early termination
-- gate network
-- model loss
-- embed loss?
-"""
+            # read
+            L = (1 - ww1 - ww2) * L + ww2 * p.unsqueeze(-1)
+            L = (1 - self.mem_one_hots).unsqueeze(0) * L  # zero out L[i, i]
+            p = (1 - ww.sum(-1, keepdim=True)) * p + ww
+            b = wr @ L
+            f = wr @ L.transpose(1, 2)
+            Kr = Kr.view(N, self.num_heads, 1, self.slot_size)
+            cr = (
+                br.unsqueeze(-1) * F.cosine_similarity(M.unsqueeze(1), Kr, dim=-1)
+            ).softmax(-1)
+            wr = Pi[0] * b + Pi[1] * cr + Pi[2] * f
+            r = (wr @ M).view(N, -1)
+
+            # act
+            x = torch.cat([hT, r], dim=-1)
+            dist = self.actor(x)  # page 7 left column
+            value = self.critic(x)
+            self.sample_new(A[t], dist)
+
+            yield RecurrentState(
+                a=A[t],
+                a_probs=dist.probs,
+                v=value,
+                h=hT,
+                r=r,
+                wr=wr,
+                u=u,
+                ww=ww,
+                M=M,
+                p=p,
+                L=L,
+            )
--- ppo/main.py
+++ ppo/main.py
@@ -67,28 +67,29 @@ def train_blocks_world(
             planner_args=None,
             **agent_args,
         ):
-            # if baseline == "dnc":
-            recurrence = dnc.Recurrence(
-                observation_space=envs.observation_space,
-                action_space=envs.action_space,
-                **dnc_args,
-                **agent_args,
-            )
-            return gntm.Agent(entropy_coef=entropy_coef, recurrence=recurrence)
-            # else:
-            #     assert baseline is None
-            #     recurrence = planner.Recurrence(
-            #         observation_space=envs.observation_space,
-            #         action_space=envs.action_space,
-            #         planning_steps=planning_steps,
-            #         **planner_args,
-            #         **agent_args,
-            #     )
-            #     return planner.Agent(
-            #         entropy_coef=entropy_coef,
-            #         model_loss_coef=model_loss_coef,
-            #         recurrence=recurrence,
-            #     )
+            if baseline == "dnc":
+                recurrence = dnc.Recurrence(
+                    observation_space=envs.observation_space,
+                    action_space=envs.action_space,
+                    **dnc_args,
+                    **agent_args,
+                )
+                return gntm.Agent(entropy_coef=entropy_coef, recurrence=recurrence)
+            else:
+                assert baseline is None
+                recurrence = planner.Recurrence(
+                    observation_space=envs.observation_space,
+                    action_space=envs.action_space,
+                    planning_steps=planning_steps,
+                    **planner_args,
+                    **dnc_args,
+                    **agent_args,
+                )
+                return planner.Agent(
+                    entropy_coef=entropy_coef,
+                    model_loss_coef=model_loss_coef,
+                    recurrence=recurrence,
+                )
 
     TrainValues(**_kwargs).run()
 
