ee84df23e68766fdc4feab668a25fb835ed2ee5f
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-21 19:03:07 -0400

    break apart planning steps and search depth

--- ppo/main.py
+++ ppo/main.py
@@ -19,9 +19,7 @@ def build_parser():
     return parsers
 
 
-def train_blocks_world(
-    increment_curriculum_at_n_satisfied, planning_steps, baseline, **_kwargs
-):
+def train_blocks_world(increment_curriculum_at_n_satisfied, baseline, **_kwargs):
     class TrainValues(Train):
         @staticmethod
         def make_env(
@@ -30,14 +28,10 @@ def train_blocks_world(
             if baseline == "dnc":
                 return dnc.Env(**env_args, seed=seed + rank)
             elif baseline == "non-recurrent":
-                return non_recurrent.Env(
-                    **env_args, planning_steps=planning_steps, seed=seed + rank
-                )
+                return non_recurrent.Env(**env_args, seed=seed + rank)
             else:
                 assert baseline is None
-                return planner.Env(
-                    **env_args, planning_steps=planning_steps, seed=seed + rank
-                )
+                return planner.Env(**env_args, seed=seed + rank)
 
         def run_epoch(self, *args, **kwargs):
             dictionary = super().run_epoch(*args, **kwargs)
@@ -87,7 +81,6 @@ def train_blocks_world(
                 recurrence = single_step.Recurrence(
                     action_space=envs.action_space,
                     observation_space=envs.observation_space,
-                    planning_steps=planning_steps,
                     **planner_args,
                     **agent_args,
                 )
@@ -103,16 +96,17 @@ def train_blocks_world(
 def blocks_world_cli():
     parsers = build_parser()
     parsers.main.add_argument("--baseline", choices=["dnc", "non-recurrent"])
-    parsers.main.add_argument("--planning-steps", type=int, default=10)
     parsers.main.add_argument("--increment-curriculum-at-n-satisfied", type=float)
     parsers.env.add_argument("--n-cols", type=int, required=True)
     parsers.env.add_argument("--curriculum-level", type=int, default=0)
     parsers.env.add_argument("--extra-time", type=int, default=0)
+    parsers.env.add_argument("--search-depth", type=int)
     parsers.agent.add_argument("--embedding-size", type=int)
     parsers.agent.add_argument("--model-loss-coef", type=float)
     planner_parser = parsers.agent.add_argument_group("planner_args")
     planner_parser.add_argument("--num-model-layers", type=int)
     planner_parser.add_argument("--num-embedding-layers", type=int)
+    planner_parser.add_argument("--planning-steps", type=int)
     dnc_parser = parsers.agent.add_argument_group("dnc_args")
     dnc_parser.add_argument("--num-slots", type=int)
     dnc_parser.add_argument("--slot-size", type=int)
