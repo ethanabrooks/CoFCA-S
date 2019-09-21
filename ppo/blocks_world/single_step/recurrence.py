2ca90fb6b4ea1113ed3b8b7d5c97d033f4aff508
Ethan Brooks <ethanabrooks@gmail.com>
2019-09-21 13:01:31 -0400

    share most layers between actor and critic

--- ppo/blocks_world/single_step/recurrence.py
+++ ppo/blocks_world/single_step/recurrence.py
@@ -23,26 +23,15 @@ class Recurrence(NNBase):
 
         init_ = lambda m: init(m, init_normc_, lambda x: nn.init.constant_(x, 0))
 
-        self.actor = nn.Sequential()
-        self.critic = nn.Sequential()
-        for i in range(num_layers):
-            in_features = num_inputs if i == 0 else hidden_size
-            self.actor.add_module(
-                name=f"fc{i}",
-                module=nn.Sequential(
-                    init_(nn.Linear(in_features, hidden_size)), activation
-                ),
-            )
-            self.critic.add_module(
-                name=f"fc{i}",
-                module=nn.Sequential(
-                    init_(nn.Linear(in_features, hidden_size)), activation
-                ),
-            )
+        layers = []
+        in_size = num_inputs
+        for _ in range(num_layers):
+            layers += [activation, init_(nn.Linear(in_size, hidden_size))]
+            in_size = hidden_size
+        self.embed1 = nn.Sequential(*layers)
 
-        self.critic_linear = init_(nn.Linear(hidden_size, 1))
-
-        self.dist = Categorical(self.output_size, action_space.n)
+        self.critic = init_(nn.Linear(hidden_size, 1))
+        self.actor = Categorical(self.output_size, action_space.n)
         self.continuous = isinstance(action_space, Box)
 
         self.train()
@@ -53,17 +42,9 @@ class Recurrence(NNBase):
         x[new] = dist.sample()[new].flatten()
 
     def forward(self, inputs, rnn_hxs, masks, action):
-        x = inputs
+        x = self.embed1(inputs)
 
-        if self.is_recurrent:
-            x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
-
-        hidden_critic = self.critic(x)
-        hidden_actor = self.actor(x)
-
-        dist = self.dist(hidden_actor)
+        dist = self.actor(x)
         self.sample_new(action, dist)
 
-        return RecurrentState(
-            a=action, probs=dist.probs, v=self.critic_linear(hidden_critic)
-        )
+        return RecurrentState(a=action, probs=dist.probs, v=self.critic(x))
