21919d4ad06c23d1398c708c08f6db03aced8b75
Ethan Brooks <ethanabrooks@gmail.com>
2019-11-05 16:20:45 -0500

    Revert "self.actor = nn.Embeddingt .from_pretrained(1e5 * torch.eye(na))"
    This reverts commit 6f7fdce10b1bcf8c6bca8a185bb4ddef8211b648.

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -63,8 +63,9 @@ class Recurrence(nn.Module):
         na = int(action_space.nvec[0])
         self.gru = nn.GRUCell(hidden_size, hidden_size)
         self.critic = init_(nn.Linear(hidden_size, 1))
+        self.actor = Categorical(2 * hidden_size, na)
         self.linear = nn.Linear(hidden_size, 2 * hidden_size)
-        self.actor = nn.Embedding.from_pretrained(1e5 * torch.eye(na))
+        self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(na))
         self.state_sizes = RecurrentState(
             a=1, a_probs=na, p=1, p_probs=na, v=1, h=hidden_size
         )
@@ -144,7 +145,8 @@ class Recurrence(nn.Module):
             self.print("dist")
             self.print(p_dist.probs)
             self.sample_new(P[t], p_dist)
-            a_dist = FixedCategorical(logits=self.actor(P[t]))
+            z = K[R, P[t]]
+            a_dist = self.actor(z)
             self.sample_new(A[t], a_dist)
             yield RecurrentState(
                 a=A[t],
