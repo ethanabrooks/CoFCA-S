82c5d13b4d7e6d97b3c232a294332f8268c8832f
Ethan Brooks <ethanabrooks@gmail.com>
2019-11-05 15:42:41 -0500

    Revert "a_dist = actor(h)"
    This reverts commit b190105d28d3566d3aabc74cb3869b5f8ac73f20.

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -60,11 +60,10 @@ class Recurrence(nn.Module):
             in_size = hidden_size
         self.f = nn.Sequential(*layers)
 
-        na = int(action_space.nvec[0])
         self.gru = nn.GRUCell(hidden_size, hidden_size)
         self.critic = init_(nn.Linear(hidden_size, 1))
-        self.actor = Categorical(hidden_size, na)
         self.linear = nn.Linear(hidden_size, 2 * hidden_size)
+        na = int(action_space.nvec[0])
         self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(na))
         self.state_sizes = RecurrentState(
             a=1, a_probs=na, p=1, p_probs=na, v=1, h=hidden_size
@@ -139,13 +138,12 @@ class Recurrence(nn.Module):
                 h = self.gru(self.f((inputs.condition[t], r)), h)
             q = self.linear(h)
             k = (K @ q.unsqueeze(2)).squeeze(2)
-            self.print("k")
+            self.print("w")
             self.print(k)
             p_dist = FixedCategorical(logits=k)
             self.print("dist")
             self.print(p_dist.probs)
-            a_dist = self.actor(h)
-            self.sample_new(A[t], a_dist)
+            self.sample_new(A[t], p_dist)
             self.sample_new(P[t], p_dist)
             yield RecurrentState(
                 a=A[t],
