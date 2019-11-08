8392f505ab8c2f1cc96ee2c208d2fb5abe28fc24
Ethan Brooks <ethanabrooks@gmail.com>
2019-11-08 10:20:05 -0500

    use task_encoder to encode transition weights

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -64,7 +64,7 @@ class Recurrence(nn.Module):
         self.gru = nn.GRUCell(hidden_size, hidden_size)
         self.critic = init_(nn.Linear(hidden_size, 1))
         self.actor = Categorical(int(sum(self.obs_sections)), na)
-        self.linear = nn.Linear(hidden_size, 2 * hidden_size)
+        self.linear = nn.Linear(hidden_size, 1)
         self.a_one_hots = nn.Embedding.from_pretrained(torch.eye(na))
         self.state_sizes = RecurrentState(
             a=1, a_probs=na, p=1, p_probs=2, v=1, h=hidden_size
@@ -107,17 +107,47 @@ class Recurrence(nn.Module):
         )
 
         # parse non-action inputs
-        # inputs = self.parse_inputs(inputs)
+        inputs = self.parse_inputs(inputs)
 
         # build memory
-        # lines = inputs.lines.view(T, N, *self.obs_spaces.lines.shape).long()[0, :, :]
-        # M = self.embeddings(lines.view(-1)).view(
-        #     *lines.shape, self.hidden_size
-        # )  # n_batch, n_lines, hidden_size
+        lines = inputs.lines.view(T, N, *self.obs_spaces.lines.shape).long()[0, :, :]
+        M = self.embeddings(lines.view(-1)).view(
+            *lines.shape, self.hidden_size
+        )  # n_batch, n_lines, hidden_size
         # forward_input = M.transpose(0, 1)  # n_lines, n_batch, hidden_size
         # K, Kn = self.task_encoder(forward_input)
         # Kn = Kn.transpose(0, 1).reshape(N, -1)
         # K = K.transpose(0, 1)
+
+        # forward_input = M.transpose(0, 1)  # n_lines, n_batch, hidden_size
+        # backward_input = forward_input.flip((0,))
+        # keys = []
+        # for i in range(len(forward_input)):
+        #     keys_per_i = []
+        #     if i > 0:
+        #         backward, _ = self.task_encoder(backward_input[:i])
+        #         keys_per_i.append(backward.flip((0,)))
+        #     if i < len(forward_input):
+        #         forward, _ = self.task_encoder(forward_input[i:])
+        #         keys_per_i.append(forward)
+        #     keys.append(torch.cat(keys_per_i).transpose(0, 1))  # put batch dim first
+        # K = torch.stack(keys, dim=1)  # put from dim before to dim
+        # K, C = torch.split(K, [self.hidden_size, 1], dim=-1)
+        # K = K.sum(dim=1)
+        # C = C.squeeze(dim=-1)
+        gru_input = M.transpose(0, 1)
+
+        K = []
+        for i in range(self.obs_sections.lines):
+            k, _ = self.task_encoder(torch.roll(gru_input, shifts=i, dims=0))
+            K.append(k)
+        S = torch.stack(K, dim=0)  # ns, ns, nb, 2*h
+
+        V = S.view(S.size(0), S.size(1), N, 2, -1)  # ns, ns, nb, 2, h
+        L = self.linear(V).squeeze(-1)  # ns, ns, nb, 2
+        L2 = L.permute(2, 0, 1, 3)  # nb, ns, ns, 2
+        L3 = L2.reshape(L2.size(0), L2.size(1), -1)  # nb, ns, 2*ns
+        P = F.softmax(L3, dim=-1)
 
         new_episode = torch.all(rnn_hxs == 0, dim=-1).squeeze(0)
         hx = self.parse_hidden(rnn_hxs)
@@ -129,7 +159,7 @@ class Recurrence(nn.Module):
         a[new_episode] = 0
         R = torch.arange(N, device=rnn_hxs.device)
         A = torch.cat([actions[:, :, 0], hx.a.view(1, N)], dim=0).long()
-        P = torch.cat([actions[:, :, 1], hx.p.view(1, N)], dim=0).long()
+        # P = torch.cat([actions[:, :, 1], hx.p.view(1, N)], dim=0).long()
 
         for t in range(T):
             # r = M[R, a]
@@ -145,7 +175,8 @@ class Recurrence(nn.Module):
             # p_dist = FixedCategorical(logits=k)
             # self.print("dist")
             # self.print(p_dist.probs)
-            a_dist = self.actor(inputs[t])
+            probs = P[R, inputs.active[t].long().squeeze(-1)]
+            a_dist = FixedCategorical(probs=probs)
             self.sample_new(A[t], a_dist)
             # a = torch.clamp(a + P[t] - self.na, 0, self.na - 1)
             # a = a + P[t]
