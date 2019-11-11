72d2ccc38b72c96347191c54b3a4da7344f77e7c
Ethan Brooks <ethanabrooks@gmail.com>
2019-11-11 10:39:40 -0500

    include action in observation. this makes env identical to the bandit branch env

--- ppo/control_flow/env.py
+++ ppo/control_flow/env.py
@@ -9,7 +9,7 @@ from rl_utils import hierarchical_parse_args, gym
 from ppo import keyboard_control
 from ppo.control_flow.lines import If, Else, EndIf, While, EndWhile, Subtask, Padding
 
-Obs = namedtuple("Obs", "condition lines")
+Obs = namedtuple("Obs", "action condition lines")
 Last = namedtuple("Last", "action active reward terminal selected")
 
 
@@ -64,9 +64,13 @@ class Env(gym.Env, ABC):
         if baseline:
             self.action_space = spaces.Discrete(n_lines)
             self.observation_space = spaces.MultiBinary(
-                2 + len(self.line_types) * n_lines
+                2 + len(self.line_types) * n_lines + (n_lines + 1)
             )
-            self.eye = Obs(condition=np.eye(2), lines=np.eye(len(self.line_types)))
+            self.eye = Obs(
+                condition=np.eye(2),
+                lines=np.eye(len(self.line_types)),
+                action=np.eye(n_lines * 2),
+            )
         else:
             self.action_space = spaces.MultiDiscrete(n_lines * np.ones(2))
             self.observation_space = spaces.Dict(
@@ -75,7 +79,7 @@ class Env(gym.Env, ABC):
                     lines=spaces.MultiDiscrete(
                         np.array([len(self.line_types)] * n_lines)
                     ),
-                    # active=spaces.Discrete(n_lines + 1),
+                    action=spaces.Discrete(n_lines * 2),
                 )
             )
         self.t = None
@@ -138,11 +142,7 @@ class Env(gym.Env, ABC):
     def get_observation(self, action):
         padded = self.lines + [Padding] * (self.n_lines - len(self.lines))
         lines = [self.line_types.index(t) for t in padded]
-        obs = Obs(
-            condition=self.condition_bit,
-            lines=lines,
-            # active=self.n_lines if self.active is None else self.active,
-        )
+        obs = Obs(condition=self.condition_bit, lines=lines, action=action)
         if self.baseline:
             obs = [eye[o].flatten() for eye, o in zip(self.eye, obs)]
             obs = np.concatenate(obs)
