7f2eb958d04f1202c7d83252f5c744cf1047c5e6
Ethan Brooks <ethanabrooks@gmail.com>
2019-11-12 13:03:48 -0500

    make while go to endwhile when failing

--- ppo/control_flow/env.py
+++ ppo/control_flow/env.py
@@ -200,7 +200,7 @@ class Env(gym.Env, ABC):
                 yield from self.get_transitions(lines_iter, current)  # from = While
             elif line is EndWhile:
                 # While
-                yield prev, current + 1  # False: While -> EndWhile
+                yield prev, current  # False: While -> EndWhile
                 yield prev, prev + 1  # True: While -> While + 1
                 # EndWhile
                 yield current, prev  # False: EndWhile -> While
