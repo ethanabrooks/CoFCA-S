3e370ec5e45e10901dbf5c13479600476ac5b7de
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-10 23:56:04 -0500

    make --world-size not required

--- ppo/control_flow/main.py
+++ ppo/control_flow/main.py
@@ -46,7 +46,7 @@ def bandit_args():
     parser.add_argument("--eval-lines", type=int, required=True)
     parser.add_argument("--no-eval", action="store_true")
     ppo.control_flow.env.build_parser(parsers.env)
-    parsers.env.add_argument("--world-size", type=int, required=True)
+    parsers.env.add_argument("--world-size", type=int)
     parsers.agent.add_argument("--debug", action="store_true")
     parsers.agent.add_argument("--no-scan", action="store_true")
     parsers.agent.add_argument("--no-roll", action="store_true")
