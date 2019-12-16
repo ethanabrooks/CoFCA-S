59f9dfacdd4304f9b5488c85f63e31675ffd984b
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-16 17:55:35 -0500

    fix bugs in scan function

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -99,20 +99,8 @@ class Recurrence(nn.Module):
         self.actor = Categorical(hidden_size, n_a)
         self.attention = Categorical(hidden_size, n_a)
         self._state_sizes = RecurrentState(
-            a=1,
-            a_probs=n_a,
-            d=1,
-            d_probs=2
-            # TODO * self.train_lines
-            ,
-            p=1,
-            v=1,
-            h=hidden_size,
+            a=1, a_probs=n_a, d=1, d_probs=2 * self.train_lines, p=1, v=1, h=hidden_size
         )
-        nl = self.obs_sections.lines
-        last = torch.zeros(1, 1, 1, self.ne)
-        last[:, :, -1] = 1
-        self.register_buffer("last", last)
 
     @property
     def gru_in_size(self):
@@ -196,7 +184,8 @@ class Recurrence(nn.Module):
         rolled = torch.cat(rolled, dim=0)
         G, H = self.task_encoder(rolled)
         H = H.transpose(0, 1).reshape(nl, N, -1)
-        last = self.last.expand(-1, N, -1, -1)
+        last = torch.zeros(nl, N, 2 * nl, self.ne, device=rnn_hxs.device)
+        last[:, :, -1] = 1
         if self.no_scan:
             P = self.beta(H).view(nl, N, -1, self.ne).softmax(2)
         else:
@@ -252,7 +241,7 @@ class Recurrence(nn.Module):
             # p_probs = torch.round(p_dist.probs * 10).flatten()
             self.sample_new(D[t], d_dist)
             n_p = d_dist.probs.size(-1)
-            p = p + D[t].clone() + 1  # TODO - n_p // 2
+            p = p + D[t].clone() - n_p // 2
             if self.clamp_p:
                 p = torch.clamp(p, min=0, max=n_p - 1)
             else:
