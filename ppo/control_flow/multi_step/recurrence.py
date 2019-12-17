c61b7470a4add01592a63dbd9f0a94112ea953cf
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-17 12:29:54 -0500

    take out if self.clamp_p block

--- ppo/control_flow/multi_step/recurrence.py
+++ ppo/control_flow/multi_step/recurrence.py
@@ -159,7 +159,10 @@ class Recurrence(ppo.control_flow.recurrence.Recurrence):
             d_dist = gate(dg, d_probs, ones * half)
             self.sample_new(D[t], d_dist)
             p = p + D[t].clone() - half
-            p = torch.clamp(p, min=0, max=nl - 1)
+            if self.clamp_p:
+                p = torch.clamp(p, min=0, max=nl - 1)
+            else:
+                p = p % nl
 
             yield RecurrentState(
                 a=A[t],
