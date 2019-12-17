cc2cfa31813231d0b21528dd33fdb3b921917f63
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-16 23:04:51 -0500

    deal with tensorflow security vulnerability

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "ray[debug]==0.7.3",
         "tensorboardX==1.8",
-        "tensorflow==1.14.0",
+        "tensorflow>=1.15",
         "opencv-python==4.1.0.25",
         "psutil==5.6.3",
         "requests==2.22.0",
