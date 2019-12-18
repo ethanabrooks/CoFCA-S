c3452c0fcd1e43348a4257b91a5807c974fa6b55
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-17 19:45:03 -0500

    revert debugging change to out_size for no_scan

--- ppo/control_flow/recurrence.py
+++ ppo/control_flow/recurrence.py
@@ -90,8 +90,7 @@ class Recurrence(nn.Module):
         for _ in range(num_encoding_layers - 1):
             layers.extend([init_(nn.Linear(in_size, encoder_hidden_size)), activation])
             in_size = encoder_hidden_size
-        # TODO: out_size = self.ne * 2 * self.train_lines if self.no_scan else self.ne
-        out_size = self.ne * 2 if self.no_scan else self.ne
+        out_size = self.ne * 2 * self.train_lines if self.no_scan else self.ne
         self.beta = nn.Sequential(*layers, init_(nn.Linear(in_size, out_size)))
 
         self.stuff = init_(nn.Linear(hidden_size, 1))
