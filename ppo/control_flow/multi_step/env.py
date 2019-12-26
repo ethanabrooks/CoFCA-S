76ee65eab20c19e721d54a7a40fa564d1d0e3467
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-26 00:37:09 -0500

    add --use-visit, make --use-monkey a bool type, and refactor how those work

--- ppo/control_flow/multi_step/env.py
+++ ppo/control_flow/multi_step/env.py
@@ -14,7 +14,7 @@ from ppo.control_flow.lines import Subtask, Padding, Line, While, If, EndWhile
 class Env(ppo.control_flow.env.Env):
     subtask_objects = ["pig", "sheep", "cat", "greenbot"]
     other_objects = ["ice", "agent"]
-    line_objects = copy(subtask_objects)
+    line_objects = subtask_objects + ["monkey"]
     world_objects = subtask_objects + other_objects
     interactions = ["pickup", "transform", "visit"]
 
