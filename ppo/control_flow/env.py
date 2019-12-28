453a3da6f055fc6b70d1974a78bb4c5f4b8f2f6d
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-28 18:52:28 +0000

    Revert "allow no_op_limit to equal len(lines) when no_op_limit is < 0"
    This reverts commit 4e20953aafbdb64af86daca1d07a955499f9bdff.

--- ppo/control_flow/env.py
+++ ppo/control_flow/env.py
@@ -185,14 +185,8 @@ class Env(gym.Env, ABC):
 
             if action == self.num_subtasks:
                 n += 1
-                if self.evaluating or self.no_op_limit is None:
-                    reached_no_op_limit = False
-                else:
-                    no_op_limit = (
-                        len(lines) if self.no_op_limit < 0 else self.no_op_limit
-                    )
-                    reached_no_op_limit = n == no_op_limit
-                if reached_no_op_limit:
+                if not self.evaluating and self.no_op_limit and n == self.no_op_limit:
+                    failing = True
                     term = True
             elif state.curr is not None:
                 step += 1
