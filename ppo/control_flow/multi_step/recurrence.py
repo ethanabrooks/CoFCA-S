74debddb3ffc647d6f68f74d5db57bf2e7cc6882
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-28 17:35:23 -0500

    use Convolution instead of linear + relu

--- ppo/control_flow/multi_step/recurrence.py
+++ ppo/control_flow/multi_step/recurrence.py
@@ -40,7 +40,28 @@ class Recurrence(ppo.control_flow.recurrence.Recurrence):
         self.gate_coef = gate_coef
         self.action_size = 4
         d = self.obs_spaces.obs.shape[0]
-        self.conv = nn.Sequential(init_(nn.Linear(d, conv_hidden_size)), nn.ReLU())
+        layers = [
+            nn.Conv2d(
+                d,
+                conv_hidden_size,
+                kernel_size=kernel_size,
+                stride=2 if kernel_size == 2 else 1,
+                padding=0,
+            ),
+            nn.ReLU(),
+        ]
+        # if kernel_size < 4:
+        # layers += [
+        # nn.Conv2d(
+        # conv_hidden_size,
+        # conv_hidden_size,
+        # kernel_size=2,
+        # stride=2,
+        # padding=0,
+        # ),
+        # nn.ReLU(),
+        # ]
+        self.conv = nn.Sequential(*layers)
         self.d_gate = Categorical(hidden_size, 2)
         self.a_gate = Categorical(hidden_size, 2)
         self.state_sizes = RecurrentState(
@@ -160,12 +181,7 @@ class Recurrence(ppo.control_flow.recurrence.Recurrence):
 
         for t in range(T):
             self.print("p", p)
-            obs = (
-                self.conv(inputs.obs[t].permute(0, 2, 3, 1))
-                .view(N, -1, self.conv_hidden_size)
-                .max(dim=1)
-                .values
-            )
+            obs = self.conv(inputs.obs[t]).view(N, -1)
             x = [
                 obs,
                 H.sum(0) if self.no_pointer else M[R, p],
