ae666facebc778e8c2186c4b2a470cd374a45025
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-29 20:07:11 +0000

    use --use-conv

--- ppo/control_flow/multi_step/recurrence.py
+++ ppo/control_flow/multi_step/recurrence.py
@@ -47,7 +47,32 @@ class Recurrence(ppo.control_flow.recurrence.Recurrence):
         self.gate_coef = gate_coef
         self.action_size = 4
         d = self.obs_spaces.obs.shape[0]
-        self.conv = nn.Sequential(init_(nn.Linear(d, conv_hidden_size)), nn.ReLU())
+        if use_conv:
+            layers = [
+                nn.Conv2d(
+                    d,
+                    conv_hidden_size,
+                    kernel_size=kernel_size,
+                    stride=2 if kernel_size == 2 else 1,
+                    padding=0,
+                ),
+                nn.ReLU(),
+            ]
+            # if kernel_size < 4:
+            # layers += [
+            # nn.Conv2d(
+            # conv_hidden_size,
+            # conv_hidden_size,
+            # kernel_size=2,
+            # stride=2,
+            # padding=0,
+            # ),
+            # nn.ReLU(),
+            # ]
+            self.conv = nn.Sequential(*layers)
+        else:
+            self.conv = nn.Sequential(init_(nn.Linear(d, conv_hidden_size)), nn.ReLU())
+
         self.d_gate = Categorical(hidden_size, 2)
         self.a_gate = Categorical(hidden_size, 2)
         self.state_sizes = RecurrentState(
@@ -167,12 +192,15 @@ class Recurrence(ppo.control_flow.recurrence.Recurrence):
 
         for t in range(T):
             self.print("p", p)
-            obs = (
-                self.conv(inputs.obs[t].permute(0, 2, 3, 1))
-                .view(N, -1, self.conv_hidden_size)
-                .max(dim=1)
-                .values
-            )
+            if self.use_conv:
+                obs = self.conv(inputs.obs[t]).view(N, -1)
+            else:
+                obs = (
+                    self.conv(inputs.obs[t].permute(0, 2, 3, 1))
+                    .view(N, -1, self.conv_hidden_size)
+                    .max(dim=1)
+                    .values
+                )
             x = [
                 obs,
                 H.sum(0) if self.no_pointer else M[R, p],
