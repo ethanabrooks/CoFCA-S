ba6ba5dc03710f3220bcd264cabc146cbc5538c6
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-30 11:08:09 -0500

    fix no_op_limit bug

--- ppo/control_flow/env.py
+++ ppo/control_flow/env.py
@@ -179,14 +179,11 @@ class Env(gym.Env, ABC):
 
             if action == self.num_subtasks:
                 n += 1
-                if self.evaluating or self.no_op_limit is None:
-                    reached_no_op_limit = False
-                else:
-                    no_op_limit = (
-                        len(lines) if self.no_op_limit < 0 else self.no_op_limit
-                    )
-                    reached_no_op_limit = n == no_op_limit
-                if reached_no_op_limit:
+                no_op_limit = self.no_op_limit
+                if self.no_op_limit is not None and self.no_op_limit < 0:
+                    no_op_limit = len(lines)
+                if not self.evaluating and n == no_op_limit:
+                    failing = True
                     term = True
             elif state.curr is not None:
                 step += 1
