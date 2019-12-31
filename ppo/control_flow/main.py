907f0cd2ee4758cc472c5664886ae0271902f434
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-31 18:02:19 +0000

    bring in changes from master but leave virtual-world-size

--- ppo/control_flow/main.py
+++ ppo/control_flow/main.py
@@ -60,7 +60,7 @@ def bandit_args():
     parsers.agent.add_argument("--debug", action="store_true")
     parsers.agent.add_argument("--no-scan", action="store_true")
     parsers.agent.add_argument("--no-roll", action="store_true")
-    parsers.agent.add_argument("--no-pointer", action="store_true")
+    parsers.agent.add_argument("--baseline")
     parsers.agent.add_argument("--include-action", action="store_true")
     parsers.agent.add_argument("--conv-hidden-size", type=int, required=True)
     parsers.agent.add_argument("--encoder-hidden-size", type=int, required=True)
