17dde6e5017c99508712282b27fe6527cba31af2
Ethan Brooks <ethanabrooks@gmail.com>
2020-01-17 15:36:02 -0500

    base interactions on objects in assign_line id

--- ppo/control_flow/multi_step/env.py
+++ ppo/control_flow/multi_step/env.py
@@ -260,10 +260,14 @@ class Env(ppo.control_flow.env.Env):
             lines, line_ids, interaction_ids, object_ids
         ):
             if line is Subtask:
-                subtask_id = (
-                    self.interactions[interaction_id],
-                    included_objects[object_id],
-                )
+                obj = included_objects[object_id]
+                if obj == self.water:
+                    interaction = self.bridge
+                elif obj == self.merchant:
+                    interaction = self.sell
+                else:
+                    interaction = self.mine
+                subtask_id = (interaction, included_objects[object_id])
                 yield Subtask(subtask_id)
             else:
                 yield line(self.objects[line_id])
