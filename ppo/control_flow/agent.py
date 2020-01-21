f82f2a8a4e039709f2130b22d5f30b96ca02861b
Ethan Brooks <ethanabrooks@gmail.com>
2020-01-21 13:27:29 -0500

    fill out else clause

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -94,8 +94,9 @@ class Agent(ppo.agent.Agent, NNBase):
                 probs = [hx.a_probs, hx.d_probs, hx.ag_probs, hx.dg_probs]
             else:
                 assert type(rm) is ppo.control_flow.recurrence.Recurrence
-                X = [hx.a, hx.d]
                 probs = [hx.a_probs, hx.d_probs]
+                X = [hx.a, hx.d]
+                aux_loss = 0
             dists = [FixedCategorical(p) for p in probs]
             action_log_probs = sum(dist.log_probs(x) for dist, x in zip(dists, X))
             entropy = sum([dist.entropy() for dist in dists]).mean()
