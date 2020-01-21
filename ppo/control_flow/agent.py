d90d689eeca5307d843e1800de74f7e504caefbf
Ethan Brooks <ethanabrooks@gmail.com>
2020-01-21 09:51:01 -0500

    add clause inside agent to handle single-step recurrence

--- ppo/control_flow/agent.py
+++ ppo/control_flow/agent.py
@@ -77,9 +77,6 @@ class Agent(ppo.agent.Agent, NNBase):
         rm = self.recurrent_module
         hx = rm.parse_hidden(all_hxs)
         a_dist = FixedCategorical(hx.a_probs)
-        probs = [hx.a_probs, hx.d_probs, hx.ag_probs, hx.dg_probs]
-        X = [hx.a, hx.d, hx.ag, hx.dg]
-        dists = [FixedCategorical(p) for p in probs]
         if type(rm) in (
             ppo.control_flow.multi_step.oh_et_al.Recurrence,
             ppo.control_flow.multi_step.no_pointer.Recurrence,
@@ -92,16 +89,25 @@ class Agent(ppo.agent.Agent, NNBase):
             else:
                 aux_loss = 0
         else:
+            if type(rm) is ppo.control_flow.multi_step.recurrence.Recurrence:
+                probs = [hx.a_probs, hx.d_probs, hx.ag_probs, hx.dg_probs]
+                X = [hx.a, hx.d, hx.ag, hx.dg]
+                assert rm.gate_coef is not None
+                assert self.no_op_coef is not None
+                aux_loss = (
+                    rm.gate_coef * (hx.ag_probs + hx.dg_probs)[:, 1].mean()
+                    + self.no_op_coef * hx.a_probs[:, -1].mean()
+                )
+            else:
+                assert type(rm) is ppo.control_flow.recurrence.Recurrence
+                probs = [hx.a_probs, hx.d_probs]
+                X = [hx.a, hx.d]
+                aux_loss = 0
+            dists = [FixedCategorical(p) for p in probs]
             action_log_probs = sum(dist.log_probs(x) for dist, x in zip(dists, X))
             entropy = sum([dist.entropy() for dist in dists]).mean()
             # action_log_probs = a_dist.log_probs(hx.a) + d_dist.log_probs(hx.d)
             # entropy = (a_dist.entropy() + d_dist.entropy()).mean()
-            assert rm.gate_coef is not None
-            assert self.no_op_coef is not None
-            aux_loss = (
-                rm.gate_coef * (hx.ag_probs + hx.dg_probs)[:, 1].mean()
-                + self.no_op_coef * hx.a_probs[:, -1].mean()
-            )
         action = torch.cat(X, dim=-1)
         aux_loss -= self.entropy_coef * entropy
         return AgentValues(
