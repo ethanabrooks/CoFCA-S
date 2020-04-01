ffef479b299a8ee1d6945a4391382c3ff0723bba
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-01 11:02:17 -0400

    add random tests

--- ppo/djikstra.py
+++ ppo/djikstra.py
@@ -1,17 +1,44 @@
+import unittest
 from collections import defaultdict
-from typing import Dict, Any, TypeVar, Set
+from typing import Dict, TypeVar, Set, List
 
+from string import ascii_lowercase
 import numpy as np
 
 X = TypeVar("X")
+Graph = Dict[X, Dict[X, float]]
 
 
-def shortest_path(_from: X, _to: X, graph: Dict[X, Dict[X, float]]):
+def dot_graph(graph: Graph):
+    def generator():
+        yield "Digraph {"
+        for node, edges in graph.items():
+            for adjacent, edge_length in edges.items():
+                yield f"{node} -> {adjacent} [label={edge_length}]"
+        yield "}"
+
+    return "\n".join(generator())
+
+
+def get_length(path: List[X], graph: Graph):
+    if not path:
+        return
+
+    def get_edge_lengths():
+        for a, b in zip(path, path[1:]):
+            yield graph[a][b]
+
+    return sum(get_edge_lengths())
+
+
+def shortest_path(_from: X, _to: X, graph: Graph):
     explored = set()
     distances = defaultdict(lambda: np.inf)
     distances[_from] = 0
     prev = {_from: None}
     while True:
+        if not distances:
+            return None
         distance, value = min(
             [(d, v) for v, d in distances.items()]
         )  # TODO: not efficient
@@ -35,7 +62,7 @@ def shortest_path(_from: X, _to: X, graph: Dict[X, Dict[X, float]]):
                 prev[adjacent] = value
 
 
-def brute_force(_from: X, _to: X, graph: Dict[X, Dict[X, float]]):
+def brute_force(_from: X, _to: X, graph: Graph):
     def get_paths(f: X, explored: Set[X]):
         if f == _to:
             yield [(f, 0)]
@@ -47,42 +74,76 @@ def brute_force(_from: X, _to: X, graph: Dict[X, Dict[X, float]]):
                         yield [(f, edge_length)] + path
 
     paths = list(get_paths(_from, explored=set()))
+    if not paths:
+        return None
     minimum = min(paths, key=lambda p: sum(d for _, d in p))
     return [x for x, _ in minimum]
 
 
-def main():
-    """
-  0_
-  |_a --1-- b
-    |    __/|
-    3 __1   3
-    |/      |
-    c --1-- d
-    """
+class TestDjikstra(unittest.TestCase):
+    def test1(self):
+        """
+      0_
+      |_a --1-- b
+        |    __/|
+        3 __1   3
+        |/      |
+        c --1-- d
+        """
 
-    graph = dict(
-        a=dict(b=1, c=3, a=0),
-        b=dict(a=1, d=3, c=1),
-        c=dict(a=3, b=1, d=1),
-        d=dict(c=1, b=3),
-    )
-    attempt = shortest_path(_from="a", _to="d", graph=graph)
-    assert (
-        attempt == brute_force(_from="a", _to="d", graph=graph) == ["a", "b", "c", "d",]
-    )
+        graph = dict(
+            a=dict(b=1, c=3, a=0),
+            b=dict(a=1, d=3, c=1),
+            c=dict(a=3, b=1, d=1),
+            d=dict(c=1, b=3),
+        )
+        attempt = shortest_path(_from="a", _to="d", graph=graph)
+        self.assertEqual(attempt, brute_force(_from="a", _to="d", graph=graph))
+        self.assertEqual(attempt, ["a", "b", "c", "d",])
 
-    """
-    a --2-- b
-    |       |
-    1       1
-    |       |
-    c --3-- d
-    """
-    graph = dict(a=dict(b=2, c=1), b=dict(a=2, d=1), c=dict(a=1, d=3), d=dict(c=3, b=1))
-    attempt = shortest_path(_from="a", _to="d", graph=graph)
-    assert attempt == brute_force(_from="a", _to="d", graph=graph) == ["a", "b", "d"]
+    def test2(self):
+        """
+        a --2-- b
+        |       |
+        1       1
+        |       |
+        c --3-- d
+        """
+        graph = dict(
+            a=dict(b=2, c=1), b=dict(a=2, d=1), c=dict(a=1, d=3), d=dict(c=3, b=1)
+        )
+        attempt = shortest_path(_from="a", _to="d", graph=graph)
+        self.assertEqual(attempt, brute_force(_from="a", _to="d", graph=graph))
+        self.assertEqual(attempt, ["a", "b", "d"])
+
+    def test_random(self):
+        np.random.seed(0)
+        alpha = ascii_lowercase[:10]
+        for _ in range(100):
+            size = np.random.randint(1, len(alpha))
+            nodes = np.random.choice(list(alpha), replace=False, size=size)
+            graph = {
+                f: {
+                    t: np.random.choice(10)
+                    for t, c in zip(nodes, np.random.choice(2, size=size))
+                    if c
+                }
+                for f in nodes
+            }
+            with self.subTest(graph=graph):
+                _from, _to = np.random.choice(nodes, size=2)
+                self.assertEqual(
+                    get_length(
+                        path=(shortest_path(_from=_from, _to=_to, graph=graph)),
+                        graph=graph,
+                    ),
+                    get_length(
+                        path=(brute_force(_from=_from, _to=_to, graph=graph)),
+                        graph=graph,
+                    ),
+                    msg=dot_graph(graph),
+                )
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
