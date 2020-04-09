4cd59de7b4557aa49630886086dc0943aa216b6b
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-09 11:29:10 -0400

    change the way that lower_level_actions are handles

--- ppo/control_flow/multi_step/env.py
+++ ppo/control_flow/multi_step/env.py
@@ -293,17 +293,34 @@ class Env(ppo.control_flow.env.Env):
                 tgt_obj = objective(*lines[ptr].id)
 
                 if type(lower_level_action) is str:
-                    done = (
-                        lower_level_action == tgt_interaction
-                        and objects.get(tuple(agent_pos), None) == tgt_obj
-                    )
+                    standing_on = objects.get(tuple(agent_pos), None)
                     if lower_level_action == self.mine:
-                        if tuple(agent_pos) in objects:
-                            if done:
-                                possible_objects.remove(objects[tuple(agent_pos)])
+                        done = (
+                            lower_level_action == tgt_interaction
+                            and standing_on == tgt_obj
+                        )
+                        if standing_on in self.items:
+                            if done or (
+                                tgt_interaction == self.sell and tgt_obj == standing_on
+                            ):
+                                possible_objects.remove(standing_on)
                             else:
                                 term = True
                             del objects[tuple(agent_pos)]
+                            inventory[standing_on] += 1
+
+                    elif lower_level_action == self.goto:
+                        done = (
+                            lower_level_action == tgt_interaction
+                            and standing_on == tgt_obj
+                        )
+                    elif lower_level_action == self.sell:
+                        if (standing_on == self.merchant) and (
+                            self.lower_level == "hardcoded" or inventory[obj] > 0
+                        ):
+                            done = True
+                        else:
+                            done = False
                     if done:
                         prev, ptr = ptr, next_subtask(ptr)
                         subtask_complete = True
