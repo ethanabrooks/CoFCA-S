2a79386c5051cc66a0eda4fd5fdb12c683f36a48
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-11 09:29:04 -0400

    bring in changes to handling of self.mine

--- ppo/control_flow/multi_step/env.py
+++ ppo/control_flow/multi_step/env.py
@@ -289,22 +289,22 @@ class Env(ppo.control_flow.env.Env):
                     lower_level_action = self.lower_level_actions[lower_level_index]
                 self.time_remaining -= 1
                 tgt_interaction, tgt_obj = lines[ptr].id
-                tgt_obj = objective(*lines[ptr].id)
 
                 if type(lower_level_action) is str:
                     standing_on = objects.get(tuple(agent_pos), None)
                     done = (
-                        lower_level_action == tgt_interaction and standing_on == tgt_obj
+                        lower_level_action == tgt_interaction
+                        and standing_on == objective(*lines[ptr].id)
                     )
                     if lower_level_action == self.mine:
                         if tuple(agent_pos) in objects:
-                            if done:
-                                possible_objects.remove(standing_on)
-                            else:
-                                term = True
                             if standing_on in self.items:
+                                if done or ((self.sell, standing_on) == lines[ptr].id):
+                                    possible_objects.remove(standing_on)
+                                else:
+                                    term = True
                                 inventory[standing_on] += 1
-                            del objects[tuple(agent_pos)]
+                                del objects[tuple(agent_pos)]
                     elif lower_level_action == self.sell:
                         done = done and (
                             self.lower_level == "hardcoded" or inventory[tgt_obj] > 0
