e6d5e33352e096ae425120437bd559d007e0c61a
Ethan Brooks <ethanabrooks@gmail.com>
2020-05-12 11:28:54 -0400

    go back to obs * h1

--- ppo/control_flow/multi_step/ours.py
+++ ppo/control_flow/multi_step/ours.py
@@ -363,14 +363,7 @@ class Recurrence(abstract_recurrence.Recurrence, recurrence.Recurrence):
                 N, self.conv_hidden_size, -1, self.kernel_size, self.kernel_size
             )
             padding = optimal_padding(self.kernel_size, self.stride)
-            conv = self.conv[0]
-            h1 = F.conv2d(
-                input=state.obs[t],
-                weight=conv.weight,
-                bias=conv.bias,
-                stride=conv.stride,
-                padding=conv.padding,
-            ) * torch.cat(
+            h1 = obs * torch.cat(
                 [
                     F.conv2d(
                         input=o.unsqueeze(0),
