1668f2492d4d35309be1020b30c926a61c0cc4c5
Ethan Brooks <ethanabrooks@gmail.com>
2020-06-06 23:46:13 -0400

    add hidden state

--- ppo/subtasks/repeat_copy.py
+++ ppo/subtasks/repeat_copy.py
@@ -5,32 +5,45 @@ from gym.utils import seeding
 
 
 class RepeatCopy(Env):
-    def __init__(self, n: int, seed=0):
+    def __init__(
+        self, n: int, h: int, seed=0,
+    ):
+        self.h = h
         self.n = n
         self.random, self.seed = seeding.np_random(seed)
         self.iterator = None
         self._render = None
         self.observation_space = gym.spaces.Box(low=np.zeros(2), high=np.ones(2))
-        self.action_space = gym.spaces.Box(low=np.zeros(1), high=np.ones(1))
+        self.action_space = gym.spaces.Box(
+            low=np.array(([0] * 3 * h) + [-1] * h + 1),
+            high=np.array(([1] * 3 * h) + [1] * h + 1),
+        )
 
     def generator(self):
-        sequence = self.random.random(self.n)
         r = 0
+        h = np.zeros(self.h)
+        c = np.zeros(self.h)
+        seq = self.random.random(self.n)
 
-        def render():
-            print(sequence)
+        def sequence():
+            for s in seq:
+                yield 0, s
+            for _ in seq:
+                yield 1, 0
 
-        self._render = render
-        for s in sequence:
-            ______ = yield (s, 0), r, False, {}
-        for s in sequence:
-            a = yield (0, 1), r, False, {}
+        for s in sequence():
+            i, f, o, g, a = yield (h, *s), r, False, {}
 
             def render():
-                print(sequence)
-                print(s, a)
+                print(seq)
+                print("i", i)
+                print("f", f)
+                print("o", o)
+                print("a", a)
 
             self._render = render
+            c = f * c + i * g
+            h = o * np.sigmoid(c)
             r = -np.abs(s - a)
         yield (0, 1), r, True, {}
 
@@ -49,4 +62,5 @@ class RepeatCopy(Env):
 
     @staticmethod
     def add_arguments(parser):
-        parser.add_argument("--n", type=int)
+        parser.add_argument("-n", type=int)
+        parser.add_argument("-h", type=int)
