7a51d37d3939ce23f717383e94020888cb3b45b6
Ethan Brooks <ethanabrooks@gmail.com>
2020-06-07 00:21:23 -0400

    add --use-cell

--- ppo/subtasks/repeat_copy.py
+++ ppo/subtasks/repeat_copy.py
@@ -5,17 +5,23 @@ from gym.utils import seeding
 
 
 class RepeatCopy(Env):
-    def __init__(self, n: int, cell_size: int, seed=0):
+    def __init__(self, n: int, cell_size: int, use_cell: bool, seed=0):
+        self.use_cell = use_cell
         self.h = h = cell_size
         self.n = n
         self.random, self.seed = seeding.np_random(seed)
         self.iterator = None
         self._render = None
+        obs_size = 2
+        action_size = 1
+        if use_cell:
+            obs_size += h
+            action_size += 4 * h
         self.observation_space = gym.spaces.Box(
-            low=np.zeros(h + 2), high=np.ones(h + 2)
+            low=np.zeros(obs_size), high=np.ones(obs_size)
         )
         self.action_space = gym.spaces.Box(
-            low=-np.ones(4 * h + 1), high=np.ones(4 * h + 1)
+            low=-np.ones(action_size), high=np.ones(action_size)
         )
 
     def generator(self):
@@ -34,23 +40,26 @@ class RepeatCopy(Env):
             return 1 / (1 + np.exp(-x))
 
         for s1, s2 in sequence():
-            obs = np.concatenate([h, np.array([s1, s2])], axis=-1)
+            obs = np.array([s1, s2])
+            obs = np.concatenate([h, obs], axis=-1) if self.use_cell else obs
             assert self.observation_space.contains(obs)
-            action = yield obs, r, False, {}
-            i, f, o, g, a = np.split(action, [self.h * i for i in range(1, 5)])
-            i, f, o = map(sigmoid, [i, f, o])
-            g = np.tanh(g)
+            a = yield obs, r, False, {}
+            if self.use_cell:
+                i, f, o, g, a = np.split(a, [self.h * i for i in range(1, 5)])
+                i, f, o = map(sigmoid, [i, f, o])
+                g = np.tanh(g)
+                c = f * c + i * g
+                h = o * 1 / (1 + np.exp(-c))
 
             def render():
                 print(seq)
-                print("i", i)
-                print("f", f)
-                print("o", o)
+                if self.use_cell:
+                    print("i", i)
+                    print("f", f)
+                    print("o", o)
                 print("a", a)
 
             self._render = render
-            c = f * c + i * g
-            h = o * 1 / (1 + np.exp(-c))
             r = -np.abs(s2 - a.item())
         yield (0, 1), r, True, {}
 
@@ -71,3 +80,4 @@ class RepeatCopy(Env):
     def add_arguments(parser):
         parser.add_argument("--n", type=int, required=True)
         parser.add_argument("--cell-size", type=int, required=True)
+        parser.add_argument("--use-cell", action="store_true")
