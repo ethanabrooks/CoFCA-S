0fe52d1c544ab77edd69bc6a0ed82cb254e5ec18
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-14 16:17:16 -0400

    update utils

--- common/vec_env/util.py
+++ common/vec_env/util.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import gym
 import gym.spaces
 import numpy as np
+import torch
 
 
 def copy_obs_dict(obs):
@@ -75,3 +76,14 @@ def obs_to_dict(obs):
     if isinstance(obs, dict):
         return obs
     return {None: obs}
+
+
+def set_seeds(cuda, cuda_deterministic, seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    cuda &= torch.cuda.is_available()
+    if cuda and cuda_deterministic:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    torch.set_num_threads(1)
