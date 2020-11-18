786e33eebcef4b5b4b054d51ba147b88f505f1e8
Ethan Brooks <ethanabrooks@gmail.com>
2020-11-18 13:11:11 -0500

    add deterministic version of MultiEmbeddingBag and log_prob_cat

--- distributions.py
+++ distributions.py
@@ -16,7 +16,18 @@ FixedCategorical = torch.distributions.Categorical
 old_sample = FixedCategorical.sample
 FixedCategorical.sample = lambda self: old_sample(self).unsqueeze(-1)
 
-log_prob_cat = FixedCategorical.log_prob
+# log_prob_cat = FixedCategorical.log_prob
+def log_prob_cat(self, value):
+    if self._validate_args:
+        self._validate_sample(value)
+    value = value.long().unsqueeze(-1)
+    value, log_pmf = torch.broadcast_tensors(value, self.logits)
+    value = value[..., :1]
+    # gather = log_pmf.gather(-1, value).squeeze(-1)
+    R = torch.arange(value.size(0))
+    return log_pmf[R, value.squeeze(-1)]  # deterministic
+
+
 FixedCategorical.log_probs = lambda self, actions: log_prob_cat(
     self, actions.squeeze(-1)
 ).unsqueeze(-1)
