02173d87409420f982e008ccf8cdc3eb437edeac
Ethan Brooks <ethanabrooks@gmail.com>
2020-11-27 07:52:12 -0500

    sanity check: just do Pylon, Assimilator, Nexus

--- data_types.py
+++ data_types.py
@@ -16,7 +16,7 @@ from utils import RESET
 
 Coord = Tuple[int, int]
 
-WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 8))
+WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 4))
 
 
 @unique
@@ -61,10 +61,10 @@ class Building(Target, WorkerAction, Enum):
     PYLON = auto()
     ASSIMILATOR = auto()
     NEXUS = auto()
-    FORGE = auto()
-    PHOTON_CANNON = auto()
-    GATEWAY = auto()
-    CYBERNETICS_CORE = auto()
+    # FORGE = auto()
+    # PHOTON_CANNON = auto()
+    # GATEWAY = auto()
+    # CYBERNETICS_CORE = auto()
     # TWILIGHT_COUNCIL = auto()
     # TEMPLAR_ARCHIVES = auto()
     # DARK_SHRINE = auto()
@@ -385,10 +385,10 @@ costs = {
     Building.NEXUS: Resources(minerals=4, gas=0),
     Building.PYLON: Resources(minerals=1, gas=0),
     Building.ASSIMILATOR: Resources(minerals=1, gas=0),
-    Building.FORGE: Resources(minerals=2, gas=0),
-    Building.GATEWAY: Resources(minerals=2, gas=0),
-    Building.CYBERNETICS_CORE: Resources(minerals=2, gas=0),
-    Building.PHOTON_CANNON: Resources(minerals=2, gas=0),
+    # Building.FORGE: Resources(minerals=2, gas=0),
+    # Building.GATEWAY: Resources(minerals=2, gas=0),
+    # Building.CYBERNETICS_CORE: Resources(minerals=2, gas=0),
+    # Building.PHOTON_CANNON: Resources(minerals=2, gas=0),
     # Building.TWILIGHT_COUNCIL: Resources(minerals=2, gas=1),
     # Building.STARGATE: Resources(minerals=2, gas=2),
     # Building.ROBOTICS_FACILITY: Resources(minerals=2, gas=1),
@@ -445,10 +445,10 @@ Symbols: Dict[WorldObject, Union[str, int]] = {
     Building.PYLON: "p",
     Building.ASSIMILATOR: "a",
     Building.NEXUS: "n",
-    Building.FORGE: "f",
-    Building.PHOTON_CANNON: "c",
-    Building.GATEWAY: "g",
-    Building.CYBERNETICS_CORE: "C",
+    # Building.FORGE: "f",
+    # Building.PHOTON_CANNON: "c",
+    # Building.GATEWAY: "g",
+    # Building.CYBERNETICS_CORE: "C",
     # Building.TWILIGHT_COUNCIL: "T",
     # Building.TEMPLAR_ARCHIVES: "A",
     # Building.DARK_SHRINE: "D",
