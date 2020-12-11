e1e05a8c4ce1c6e244a30c6adb8bb3639f60c1fd
Ethan Brooks <ethanabrooks@gmail.com>
2020-12-11 00:07:12 -0500

    do not limit the size of agent movements

--- data_types.py
+++ data_types.py
@@ -337,8 +337,9 @@ Assignment = Union[BuildOrder, Resource]
 
 class MovementType(type):
     def __iter__(self):
-        for i in range(-1, 2):
-            for j in range(-1, 2):
+        max_steps = 2  # TODO
+        for i in range(-max_steps, max_steps):
+            for j in range(-max_steps, max_steps):
                 yield Movement(i, j)
 
 
@@ -349,7 +350,7 @@ class Movement(WorkerAction, metaclass=MovementType):
 
     @classmethod
     def from_(cls, origin, to):
-        return cls(*np.clip(np.array(to) - np.array(origin), -1, 1))
+        return cls(*np.array(to) - np.array(origin))
 
 
 WorkerActions = [*Buildings, *Movement]
