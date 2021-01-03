ee269e2039f829faecc7fb4a32aeba96f12bfdd0
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-02 21:44:59 -0500

    set gas cost to 0 for all buildings

--- data_types.py
+++ data_types.py
@@ -868,7 +868,7 @@ class CyberneticsCore(Building):
 class DarkShrine(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=150, gas=150)
+        return Resources(minerals=150, gas=0)  # 150)
 
     @property
     def symbol(self) -> str:
@@ -878,7 +878,7 @@ class DarkShrine(Building):
 class FleetBeacon(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=300, gas=200)
+        return Resources(minerals=300, gas=0)  # 200)
 
     @property
     def symbol(self) -> str:
@@ -938,7 +938,7 @@ class Pylon(Building):
 class RoboticsBay(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=200, gas=200)
+        return Resources(minerals=200, gas=0)  # 200)
 
     @property
     def symbol(self) -> str:
@@ -948,7 +948,7 @@ class RoboticsBay(Building):
 class RoboticsFacility(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=200, gas=100)
+        return Resources(minerals=200, gas=0)  # 100)
 
     @property
     def symbol(self) -> str:
@@ -958,7 +958,7 @@ class RoboticsFacility(Building):
 class StarGate(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=150, gas=150)
+        return Resources(minerals=150, gas=0)  # 150)
 
     @property
     def symbol(self) -> str:
@@ -968,7 +968,7 @@ class StarGate(Building):
 class TemplarArchives(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=150, gas=200)
+        return Resources(minerals=150, gas=0)  # 200)
 
     @property
     def symbol(self) -> str:
@@ -978,7 +978,7 @@ class TemplarArchives(Building):
 class TwilightCouncil(Building):
     @property
     def cost(self) -> Resources:
-        return Resources(minerals=150, gas=100)
+        return Resources(minerals=150, gas=0)  # 100)
 
     @property
     def symbol(self) -> str:
@@ -988,17 +988,17 @@ class TwilightCouncil(Building):
 Buildings: List[Building] = [
     # Assimilator(),
     CyberneticsCore(),
-    # DarkShrine(),
-    # FleetBeacon(),
+    DarkShrine(),
+    FleetBeacon(),
     Forge(),
-    # Gateway(),
+    Gateway(),
     Nexus(),
     PhotonCannon(),
     Pylon(),
-    # RoboticsBay(),
-    # RoboticsFacility(),
-    # StarGate(),
-    # TemplarArchives(),
-    # TwilightCouncil(),
+    RoboticsBay(),
+    RoboticsFacility(),
+    StarGate(),
+    TemplarArchives(),
+    TwilightCouncil(),
 ]
 WorldObjects = list(Buildings) + list(Resource) + list(Worker)
