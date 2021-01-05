4cc6c3ab82f84dfe9ca6c29e07c5b527f301785c
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-05 10:22:02 -0500

    clean up bin and add fix_config

--- bin/get_dir.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-pattern=${1-'--active'}
-sessions=(${(f)"$(runs ls $pattern)"})
-echo "${sessions[1]%/[0-9]*}"
--- bin/single-process.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-sed 's/batch-size="\{0,1\}[^ "]*"\{0,1\}/batch-size=1/' |\
-sed 's/num-processes="\{0,1\}[^ "]*"\{0,1\}/num-processes=1/'
