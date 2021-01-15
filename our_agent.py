0c080fdef00ff20e848486c0e6b5fc27e6c97fc9
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-15 01:30:57 -0500

    add feed_m_to_gru argument

--- our_agent.py
+++ our_agent.py
@@ -55,6 +55,7 @@ class AgentConfig:
     add_layer: bool = True
     conv_hidden_size: int = 100
     debug: bool = False
+    feed_m_to_gru: bool = True
     gate_coef: float = 0.01
     globalized_critic: bool = False
     instruction_embed_size: int = 128
@@ -78,6 +79,7 @@ class Agent(NNBase):
     action_space: spaces.MultiDiscrete
     conv_hidden_size: int
     debug: bool
+    feed_m_to_gru: bool
     gate_coef: float
     globalized_critic: bool
     hidden_size: int
@@ -226,7 +228,9 @@ class Agent(NNBase):
         )
 
     def get_gru_in_size(self):
-        return self.instruction_embed_size + self.action_embed_size
+        return (
+            self.instruction_embed_size if self.feed_m_to_gru else 0
+        ) + self.action_embed_size
 
     def build_d_gate(self):
         return self.init_(nn.Linear(self.z_size, 2))
@@ -340,9 +344,12 @@ class Agent(NNBase):
             state.partial_action.long()
         )  # +1 to deal with negatives
         m = self.build_m(M, R, p)
-        h, rnn_hxs = self._forward_gru(
-            torch.cat([m, embedded_action], dim=-1), rnn_hxs, masks
+        gru_in = (
+            torch.cat([m, embedded_action], dim=-1)
+            if self.feed_m_to_gru
+            else embedded_action
         )
+        h, rnn_hxs = self._forward_gru(gru_in, rnn_hxs, masks)
         z1 = torch.cat([x, resources, embedded_action, h], dim=-1)
 
         _z = z1.unsqueeze(1).expand(-1, rolled.size(1), -1)
