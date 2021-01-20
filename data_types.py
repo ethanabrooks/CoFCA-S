6e3f0f44757529529011624bd88e4b2e4029b280
Ethan Brooks <ethanabrooks@gmail.com>
2021-01-20 10:06:51 -0500

    prevent mining of gas without Assimilator in assignment method

--- data_types.py
+++ data_types.py
@@ -547,7 +547,9 @@ class ActionStage:
         pass
 
     @abstractmethod
-    def assignment(self, positions: Positions) -> Optional[Assignment]:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Optional[Assignment]:
         pass
 
     def from_input(self) -> "ActionStage":
@@ -649,7 +651,9 @@ class NoWorkersAction(ActionStage):
             return NoWorkersAction()
         return WorkersAction(workers)
 
-    def assignment(self, positions: Positions) -> Optional[Assignment]:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Optional[Assignment]:
         return DoNothing()
 
     def get_workers(self) -> WorkerGenerator:
@@ -714,7 +718,9 @@ class WorkersAction(HasWorkers, CoordCanOpenGate):
             return BuildingAction(workers=self.workers, building=action.building)
         raise RuntimeError
 
-    def assignment(self, positions: Positions) -> Optional[Assignment]:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Optional[Assignment]:
         return None
 
 
@@ -725,11 +731,17 @@ class CoordAction(HasWorkers, NoWorkersAction):
     def action_components(self) -> CompoundAction:
         return replace(HasWorkers.action_components(self), coord=self.coord)
 
-    def assignment(self, positions: Positions) -> Optional[Assignment]:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Optional[Assignment]:
         i, j = astuple(self.coord)
         for resource in Resource:
             if resource.on((i, j), positions):
-                return resource
+                if (
+                    resource is not Resource.GAS
+                    or building_positions.get((i, j)) == Assimilator()
+                ):
+                    return resource
         return GoTo((i, j))
 
 
@@ -766,7 +778,9 @@ class BuildingAction(HasWorkers, CoordCanOpenGate):
     def action_components(self) -> CompoundAction:
         return replace(HasWorkers.action_components(self), building=self.building)
 
-    def assignment(self, positions: Positions) -> Optional[Assignment]:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Optional[Assignment]:
         return None
 
     def invalid(
@@ -797,7 +811,9 @@ class BuildingCoordAction(HasWorkers, NoWorkersAction):
             HasWorkers.action_components(self), coord=self.coord, building=self.building
         )
 
-    def assignment(self, positions: Positions) -> Assignment:
+    def assignment(
+        self, positions: Positions, building_positions: BuildingPositions
+    ) -> Assignment:
         i, j = astuple(self.coord)
         on_gas = Resource.GAS.on((i, j), positions)
         assimilator = isinstance(self.building, Assimilator)
